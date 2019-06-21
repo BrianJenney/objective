ae063ec1af1a2c7ff1143cc8ead2039db1528704
joey <joey@jxlab.io>
2019-06-21 11:00:07 -0400

    Removed store dir, moved index.js to store.js in root dir

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
-import reducers from '../reducers';
+import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
