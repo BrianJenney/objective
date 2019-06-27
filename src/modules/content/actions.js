af3d1ba03a6627d2f0752804c9b6fe3cec365aea
joey <joey@jxlab.io>
2019-06-27 01:22:17 -0400

    Fixed bug where url for static content was not being pulled in through proper parameters

--- src/modules/content/actions.js
+++ src/modules/content/actions.js
@@ -3,14 +3,13 @@ import { REQUEST_FETCH_CONTENT, RECEIVED_FETCH_CONTENT } from './types';
 const msgpack = require('msgpack-lite');
 const ObjectId = require('bson-objectid');
 
-export const requestFetchContent = contentId => async (dispatch, getState) => {
+export const requestFetchContent = url => async (dispatch, getState) => {
   const stompClient = getState().stomp.client;
   const replyTo = getState().stomp.replyTo;
-  let url = window.location.pathname;
   const params = {
     'params': {
       'query': {
-        'contentPage': url
+        'slug': url
       }
     }
   };
