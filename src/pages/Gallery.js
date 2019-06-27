5d106989330c078f6d0657f160ab892fe34825cd
Rinor Sadiku <rinors@nutranext.net>
2019-06-26 23:59:58 -0400

    Gallery working

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,53 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container } from 'semantic-ui-react';
+import { Container, Segment } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import { requestFetchProducts } from '../modules/products/actions';
 
 class Gallery extends React.Component {
   componentWillMount() {
     this.props.requestFetchProducts(['5ceea2eb0023ee3bcc730cc7', '5ceebf7ea686a03bccfa67bf', '5ceec48fa686a03bccfa67c4']);
-    //'5ceea2eb0023ee3bcc730cc7', '5ceebf7ea686a03bccfa67bf', '5ceec48fa686a03bccfa67c4'
   }
   
   render() {
-    console.log('ppppppppproooooooooooooooddss');
-
     if(!this.props.products){
       return (<div></div>);
     }
 
-
     let prods = this.props.products;
-    console.log(prods);
+    let prodlist = Object.values(prods).map((product, ind) => (
+      <Container key={product._id}>
+          <Segment>
+            <h3>{product.name}</h3>
+            SKU: {product.sku}<br/>
+            {product.description}<br/>
+            Price: {product.price.$numberDecimal}
+          </Segment>
+      </Container>
+    ));
+    
     return(
-      "this"
+        prodlist
     )
-    // let prodlist = this.props.products.map(product=>{
-    //   return (
-    //     <Container key={product._id}>
-    //       <p>{product.name}</p>
-    //     </Container>
-    //    );
-    // })
-
-    // let prodList = this.props.products.map(function(p) {
-    //   return (
-    //       <p> {p.name}</p>
-    //   );
-    // })
- 
-    // return (
-    //   <Container>
-    //     {prodList}
-    //   </Container>
-    // );
   }
 }
 
 const mapStateToProps = state => {
-  console.log("stattttte");
-  console.log(state);
   return {
     products: state.products
   };
