8dc66276530c1955290f3ac12787b19353537d06
Zaneh Williams <zanehw@nutranext.net>
2019-07-02 13:25:36 -0400

    working connection

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Grid, Card } from 'semantic-ui-react';
 import { requestFetchAccount} from '../modules/account/actions';
 
 class Account extends React.Component {
   componentWillMount() {
-    this.props.requestFetchAccount(process.env.REACT_APP_STORE_CODE);
+    this.props.requestFetchAccount('5cdc7405da53494ee0f3bafe');
   }
 
   render() {
+    if (!this.props.account.users) {
+      return (<div></div>);
+    }
+    let users = this.props.account.users;
+    var userNum = 1;
+    let userList = Object.values(users).map((user) => (
+      <Grid.Column className="card" key={userNum++}>
+      <Card>
+        <Card.Content>
+          <Card.Header>{user.firstName} {user.lastName}</Card.Header>
+          <Card.Description>
+            <p>Email: {user.email}</p>
+            <p>Preferred Contact Method: {user.contactPreferences.preferred}</p>
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    </Grid.Column>
+    ));
+
     return (
       <Container>
         <Header as='h2'>Account Info</Header>
-        <p>{this.props.account}</p>
+        <p>Status: {this.props.account.status}</p>
+        <Grid>
+          {userList}
+        </Grid>
       </Container>
     )
   }
--- src/store.js
+++ src/store.js
@@ -5,14 +5,14 @@ import stompReducer from './modules/stomp/reducers';
 import storefrontReducer from './modules/storefront/reducers';
 import contentReducer from './modules/content/reducers';
 import productReducer from './modules/products/reducers';
-import userReducer from './modules/account/reducers';
+import accountReducer from './modules/account/reducers';
 
 const rootReducer = combineReducers({
   stomp: stompReducer,
   storefront: storefrontReducer,
   content: contentReducer,
   products: productReducer,
-  users: userReducer
+  account: accountReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
