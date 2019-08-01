016529591b7453339238c137ba381a4ee00cee7f
Emily Kwak <emilykwak1@gmail.com>
2019-08-01 07:43:27 -0700

    implemented fbemitter

--- src/events.js
+++ src/events.js
@@ -1,17 +1,4 @@
-const EventEmitter = {
-  events: {},
-  dispatch: function (event, data) {
-    if (!this.events[event])
-      return;
+const { EventEmitter } = require('fbemitter');
+const emitter = new EventEmitter()
 
-    this.events[event].forEach(callback => callback(data));
-  },
-  subscribe: function (event, callback) {
-    if (!this.events[event])
-      this.events[event] = [];
-
-    this.events[event].push(callback);
-  }
-};
-
-module.exports = { EventEmitter };
+export default emitter;
