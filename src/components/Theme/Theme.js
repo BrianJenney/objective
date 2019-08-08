8e0e9d89af312f8a46258b5453b8af957b155d14
John Alexis Franka <john-alexisf@usmdbthlt-6lvcg.npi.com>
2019-08-08 14:50:30 -0400

    created initial fonts const and added fonts to theme

--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -36,6 +36,72 @@ const nxtTheme = createMuiTheme({
       main: colors.red,
       dark: colors.red,
       contrastText: colors.white
+import { fonts, backupFontStack, sizes } from './fonts';
+
+const nxtTheme = createMuiTheme({
+  typography: {
+    htmlFontSize: 16,
+    fontSize: 16,
+    fontFamily: `${fonts.body}, ${backupFontStack.sans}`,
+    h1: {
+      fontFamily: `${fonts.header}, ${backupFontStack.sans}`,
+      fontSize: sizes.h1,
+      fontWeight: 800,
+      lineHeight: '3.125rem'
+    },
+    h2: {
+      fontFamily: `${fonts.header}, ${backupFontStack.sans}`,
+      fontSize: sizes.h2,
+      fontWeight: 800,
+      lineHeight: '1.875rem'
+    },
+    h3: {
+      // Title T1
+      fontFamily: `${fonts.header}, ${backupFontStack.sans}`,
+      fontSize: sizes.title,
+      fontWeight: 800,
+      lineHeight: '1.4375rem'
+    },
+    h4: {
+      // subhead S1
+      fontFamily: `${fonts.caption}, ${backupFontStack.sans}`,
+      fontSize: sizes.subhead,
+      fontWeight: 400,
+      lineHeight: '1.4375rem'
+    },
+    subtitle1: {
+      // subhead S1
+      fontFamily: `${fonts.caption}, ${backupFontStack.sans}`,
+      fontSize: sizes.subhead,
+      fontWeight: 400,
+      lineHeight: '1.4375rem'
+    },
+    caption: {
+      fontFamily: `${fonts.caption}, ${backupFontStack.sans}`,
+      fontSize: sizes.caption,
+      fontWeight: 400,
+      lineHeight: '0.9375rem'
+    },
+    button: {
+      fontFamily: `${fonts.header}, ${backupFontStack.sans}`,
+      textTransform: 'uppercase',
+      textDecoration: 'none',
+      color: colors.white
+    },
+    body1: {
+      fontFamily: `${fonts.body}, ${backupFontStack.sans}`,
+      fontSize: sizes.body,
+      fontWeight: 400,
+      lineHeight: '1.4375rem'
+    }
+  },
+  overrides: {},
+  shape: {
+    borderRadius: 0
+  },
+  props: {
+    MuiButtonBase: {
+      disableRipple: true
     }
   }
 });
---
+++ src/components/Theme/fonts.js
@@ -0,0 +1,20 @@
+export const fonts = {
+  body: 'Georgia',
+  header: 'Canela Text',
+  caption: 'Helvetica'
+};
+
+export const backupFontStack = {
+  sans: 'Helvetica, Arial, sans-serif'
+};
+
+export const sizes = {
+  h1: '2.75rem',
+  h2: '1.625rem',
+  title: '1rem',
+  subhead: '1rem',
+  body: '1rem',
+  caption: '0.75rem',
+  tableHead: '0.75rem',
+  tableBody: '0.875rem'
+};
