31f0a44ebf5b4961f8ed5a47d98be2f042abad75
John Alexis Franka <john-alexisf@usmdbthlt-6lvcg.npi.com>
2019-08-08 10:50:02 -0400

    added theme, added variables and colors. Wrapped app with theme provider

---
+++ src/components/Theme/nxt-palette.js
@@ -0,0 +1,19 @@
+export const nxtPalette = {
+  // primary colors
+FOREST_GREEN: '#003833',
+SEA_FOAM: '#c3f1cf',
+SIENNA: '#87331f',
+PALE_PINK: '#FDE1DE',
+NAVY: '#1F396D',
+LIGHT_BLUE: '#DAEFF1',
+PLUM: '#621244',
+LAVENDER: '#DED1E1',
+BLACK: '#000000',
+OFF_WHITE: '#FCF8F4',
+
+// secondary colors
+LEMON: '#FFE671',
+ORANGE: '#FABA91',
+VANILLA: '#FEF6D3',
+
+}
