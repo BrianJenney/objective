399813627ec189790e87f52cd69185cce9416276
Rinor Sadiku <rinors@nutranext.net>
2019-08-08 14:59:28 -0400

    Reusable component for PDP cards

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -4,6 +4,7 @@ import { ProductStore } from '../contexts/ProductContext';
 
 import ProductHeader from './product/ProductHeader';
 import VariantSelectionForm from './product/VariantSelectionForm';
+import PDPCard from './product/PDPCard';
 
 class Product extends Component {
   render() {
@@ -11,6 +12,8 @@ class Product extends Component {
       <ProductStore productId={this.props.match.params.id}>
         <ProductHeader />
         <VariantSelectionForm />
+        <PDPCard title='My title is awesome' cardType='imgcard1' body='this is body' />
+        <PDPCard title='imgcard 2 title' cardType='imgcard2' body='img card 2 body' />
       </ProductStore>
     );
   }
---
+++ src/pages/product/CardTypes.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+
+
+const imgcard1 = (title, body) => {
+
+  const Styles = makeStyles({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+  const classes = Styles();
+
+  return (
+    <>
+      <CardMedia
+        className={classes.media}
+        image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
+        title="Contemplative Reptile" />
+      <CardContent>
+        {title}
+        {body}
+      </CardContent>
+    </>
+
+  );
+};
+
+const imgcard2 = (title, body, icon) => {
+
+  const Styles = makeStyles({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+  const classes = Styles();
+
+  return (
+    <>
+      <CardMedia
+        className={classes.media}
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAaVvMW5zQqL7iu3UDSrgBazRKnbTOA6MKGl7Xhe6XODUhLRIQ"
+        title="Contemplative Reptile" />
+      <CardContent>
+        {title}
+        {body}
+      </CardContent>
+    </>
+
+  );
+};
+
+
+export { imgcard1, imgcard2 };
