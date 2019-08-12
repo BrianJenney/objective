837021088d50740b2a489bee2dd7669830d35905
John Alexis Franka <john-alexisf@usmdbthlt-6lvcg.npi.com>
2019-08-12 16:44:27 -0400

    DC-154 DC-161 DC162 - Created Slider component using MUI API. Created Best Seller component with SVG star

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -3,14 +3,12 @@ import React, { Component } from 'react';
 import { GalleryStore } from '../contexts/GalleryContext';
 
 import ProductCard from './gallery/ProductCard';
-import Slider from '../components/Slider/';
 
 class Gallery extends Component {
   render() {
     return (
       <GalleryStore productIds={['5ceea2eb0023ee3bcc730cc7', '5ceebf7ea686a03bccfa67bf', '5ceec48fa686a03bccfa67c4']}>
         <ProductCard></ProductCard>
-        <Slider />
       </GalleryStore>
     );
   }
