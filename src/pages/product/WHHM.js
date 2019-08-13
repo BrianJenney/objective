de1d57169b124d6e20a6ca63a7e7560a8f8c9026
Emily Kwak <emilyk@usmdbthlt-8lvcg.npi.com>
2019-08-13 10:50:41 -0400

    added grid to component

---
+++ src/pages/product/WHHM.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import PDPCard from './PDPCard'
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function WHHM() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}><PDPCard title='TEST' cardType='imgcard3' body='test' /></Paper>
+          <Typography align='center'>
+            TEST
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
