0d5e616031492e8160e91d4758d685f6c01baade
Kevin <kevinch@nutranext.net>
2019-08-14 11:29:03 -0400

    Incorporating Aisha's chamges for mobile - sans Promise section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -123,7 +123,7 @@ const Footer = () => {
             <Grid item xs={6} className={classes.footerBoxMid}>
               <Grid container spacing={0}>
                 <Grid item xs={12}>
-                  Shop
+                  Help
                 </Grid>
                 <Grid item xs={12}>
                   <Box fontSize={11} className={classes.aboutLink}>
