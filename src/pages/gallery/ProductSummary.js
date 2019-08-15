e32f825c14a85eb17b67f7277744bc6467ef2436
Joey Shakespeare <joeys@nutranext.net>
2019-08-15 03:54:58 +0000

    [CODE FORMAT] Fixing code formatting

--- src/pages/gallery/ProductSummary.js
+++ src/pages/gallery/ProductSummary.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 
-const ProductSummary = ({product}) => {
+const ProductSummary = ({ product }) => {
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card>
         <CardMedia
-          style={{height: 355, width: 200, margin: '20px 90px'}}
+          style={{ height: 355, width: 200, margin: '20px 90px' }}
           image={product.assets.imgs}
           title={product.name}
         />
