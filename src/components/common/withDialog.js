96e76fc95608a84cfae40cd3854aab2d9be535ae
Daniel Ha <danielha@nutranext.net>
2019-08-15 19:45:20 -0700

    Create withDialog as HOC to display a dialog

---
+++ src/components/common/withDialog.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+
+const withDialog = (WrappedComponent) => {
+  const  WithDialog = (props) => {
+    const [open, setOpen] = useState(false);
+    useEffect(() => {
+      setOpen(true);
+    },[]);
+    const handleDialogClose = useCallback(() => {
+      setOpen(false);
+    },[]);
+    return (
+      <Dialog
+        open={open}
+        onClose={handleDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        disableBackdropClick={true}
+        disableEscapeKeyDown={true}
+        onExited={props.onExited}
+      >
+        <WrappedComponent closeDialog={handleDialogClose} {...props} />
+      </Dialog>
+    );
+  };
+  return WithDialog;
+};
+
+export default withDialog;
