dda2fdcabf8dd09dc1ba6b68eb3cd8be73d84f94
Alex Franka <alexfranka@gmail.com>
2019-08-19 11:31:03 -0400

    Deleted old Slider folder from components. Moved ProductSlider to the ProductDetail page. Edited styles to center product on slider

--- src/pages/product/ProductDetail.js
+++ src/pages/product/ProductDetail.js
@@ -1,25 +1,22 @@
 import React, { useContext, useCallback } from 'react';
-
 import ProductContext from '../../contexts/ProductContext';
-
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
+import ProductSlider from '../../components/common/Slider';
 
 import { useQuantity } from '../../hooks';
 
 const useStyles = makeStyles(theme => ({
-  media: {
-    width: 200,
-    height: 355,
-    margin: '20px 90px'
+  root: {
+    width: '100%',
+    maxWidth: '728px'
   }
 }));
 
@@ -42,14 +39,10 @@ const ProductDetail = () => {
     <Container>
       <Card >
         <Grid container spacing={4}>
-          <Grid item xs={12} sm={4} md={4} alignItems="center">
-            <CardMedia
-              className={classes.media}
-              image={product.assets.imgs}
-              title={product.name}
-            />
+          <Grid item xs={12} sm={6}>
+            <ProductSlider />
           </Grid>
-          <Grid item item xs={12} sm={8} md={8}>
+          <Grid item xs={12} sm={6}>
             <CardContent>
               <Typography gutterBottom variant="h4" align="center">{product.name}</Typography>
               <Divider variant="fullWidth" />
