b1a13817d6aab74787aa9e2b7b19639e0d0388e6
Daniel Ha <danielha@nutranext.net>
2019-08-27 10:19:38 -0700

    Show item count in shopping bag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { withStyles, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Grid, Box, Link } from '@material-ui/core';
+import Badge from '@material-ui/core/Badge/Badge';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import { Link as RouterLink } from 'react-router-dom';
 import CloseIcon from '@material-ui/icons/Close';
@@ -10,6 +12,7 @@ import TemporaryDrawer from './common/TemporaryDrawer';
 import CartDrawer from '../pages/cart/CartDrawer';
 import './Header-style.scss';
 import ShoppingBag from './common/Icons/Shopping-Bag';
+
 
 const StyledLink = withStyles(() => ({
   root: {
@@ -33,6 +36,17 @@ const StyledBox = withStyles(() => ({
     lineHeight: '44px'
   }
 }))(Box);
+
+const StyledBadge = withStyles(theme => ({
+  badge: {
+    top: '30%',
+    right: -3,
+    // The border color match the background color.
+    border: `2px solid ${
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[900]
+    }`,
+  },
+}))(Badge);
 
 const Header = () => {
   const cart = useSelector(state => state.cart);
@@ -157,28 +171,3 @@ const Header = () => {
 };
 
 export default Header;
-const renderCartIcon = () => {
-  const cartCount = cart.items.length;
-  const CartIcon = () => (
-    <SvgIcon>
-      <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
-    </SvgIcon>
-  );
-  return (
-    <TemporaryDrawer
-      toggleContent={
-        <StyledBadge invisible={cartCount < 1} badgeContent={cartCount} color="secondary"><CartIcon /></StyledBadge>
-      }
-      closer={
-        <StyledBox
-          position="absolute"
-          right={10}
-          top={10}
-          children={<CloseIcon />}
-        />
-      }
-      listContent={<CartDrawer />}
-      side="right"
-    ></TemporaryDrawer>
-  );
-};
