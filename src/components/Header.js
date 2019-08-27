1ae9e0a785c3a898ce75e79dc4eb02cf7e7eb1fd
Daniel Ha <danielha@nutranext.net>
2019-08-27 09:58:36 -0700

    Add item count to shopping bag

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,6 +35,8 @@ const StyledBox = withStyles(() => ({
 }))(Box);
 
 const Header = () => {
+  const cart = useSelector(state => state.cart);
+
   const renderBurgerIcon = () => (
     <>
       <DropdownMenu
@@ -54,16 +56,23 @@ const Header = () => {
     </>
   );
 
-  const renderCartIcon = () => (
-    <TemporaryDrawer
-      toggleContent={<ShoppingBag />}
-      closer={
-        <Box position="absolute" right={10} top={10} children={<CloseIcon />} />
-      }
-      listContent={<CartDrawer />}
-      side="right"
-    ></TemporaryDrawer>
-  );
+  const renderCartIcon = () => {
+    const cartCount = cart.items.length;
+    return (
+      <TemporaryDrawer
+        toggleContent={
+          <StyledBadge invisible={cartCount < 1} badgeContent={cartCount} color="secondary">
+            <ShoppingBag />
+          </StyledBadge>
+        }
+        closer={
+          <Box position="absolute" right={10} top={10} children={<CloseIcon />} />
+        }
+        listContent={<CartDrawer />}
+        side="right"
+      ></TemporaryDrawer>
+    );
+  };
 
   const theme = useTheme();
   const burger = useMediaQuery(theme.breakpoints.down('xs'));
@@ -148,3 +157,28 @@ const Header = () => {
 };
 
 export default Header;
+const renderCartIcon = () => {
+  const cartCount = cart.items.length;
+  const CartIcon = () => (
+    <SvgIcon>
+      <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
+    </SvgIcon>
+  );
+  return (
+    <TemporaryDrawer
+      toggleContent={
+        <StyledBadge invisible={cartCount < 1} badgeContent={cartCount} color="secondary"><CartIcon /></StyledBadge>
+      }
+      closer={
+        <StyledBox
+          position="absolute"
+          right={10}
+          top={10}
+          children={<CloseIcon />}
+        />
+      }
+      listContent={<CartDrawer />}
+      side="right"
+    ></TemporaryDrawer>
+  );
+};
