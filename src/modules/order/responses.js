ce310e2ff18fda1205ca699146867d67c97463e9
Rinor Sadiku <rinors@nutranext.net>
2019-08-27 22:06:04 -0400

    Clear cart on a success order

--- src/modules/order/responses.js
+++ src/modules/order/responses.js
@@ -1,17 +1,28 @@
 import store from '../../store';
 import { receivedCreateOrder } from './actions';
+import { requestCreateCart } from '../cart/actions';
 
 export const handleOrderResponse = (status, data, fields, properties) => {
   switch (fields.routingKey) {
-  case 'order.request.create':
-    console.log('****************** Order Create Response ******************');
-    console.log(status);
-    console.log(data);
-    console.log(fields);
-    console.log(properties);
-    store.dispatch(receivedCreateOrder(data));
-    break;
-  default:
-    console.log('bad response ' + fields.routingKey);
+    case 'order.request.create':
+      console.log('****************** Order Create Response ******************');
+      console.log(status);
+      console.log(data);
+      console.log(fields);
+      console.log(properties);
+      store.dispatch(receivedCreateOrder(data));
+
+      // status handling
+      switch (status) {
+        case 'success':
+          // clear cart on success
+          store.dispatch(requestCreateCart());
+          break;
+        default:
+          console.log('unknown status ' + status);
+      }
+      break;
+    default:
+      console.log('bad response ' + fields.routingKey);
   }
 };
