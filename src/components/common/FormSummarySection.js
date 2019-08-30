88b580455dfba492aca27972343f385739e78946
Leonardo Kim <luckkim87729@hotmail.com>
2019-08-29 20:48:49 -0400

    DC-247: Implement editable panel and other common components

---
+++ src/components/common/FormSummarySection.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Typography } from '@material-ui/core';
+
+const FormSummarySection = ({ title, pairs, children, ...rest }) => (
+  <Box {...rest}>
+    {title && <Typography variant="h3" children={title} gutterBottom />}
+    {children}
+    {pairs && (
+      <Box>
+        {pairs.map(({ label, value, ...childProps }, index) => (
+          <Box
+            key={`summary_${index}`}
+            display="flex"
+            alignItems="center"
+            mb={1}
+            {...childProps}
+          >
+            {label ? (
+              <>
+                <Box
+                  component={Typography}
+                  width={1 / 3}
+                  mr={2}
+                  variant="subtitle2"
+                  children={`${label}:`}
+                />
+                <Box
+                  flex={1}
+                  component={Typography}
+                  variant="body2"
+                  children={value}
+                />
+              </>
+            ) : (
+              value
+            )}
+          </Box>
+        ))}
+      </Box>
+    )}
+  </Box>
+);
+
+FormSummarySection.propTypes = {
+  title: PropTypes.string.isRequired,
+  pairs: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      value: PropTypes.node
+    })
+  ),
+  children: PropTypes.node
+};
+
+export default FormSummarySection;
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,6 +1,9 @@
 export { default as AlertPanel } from './AlertPanel';
 export { default as Button } from './Button';
 export { default as Carousel } from './Carousel';
+export { default as Panel } from './Panel';
+export { default as EditablePanel } from './EditablePanel';
+export { default as FormSummarySection } from './FormSummarySection';
 export { default as DropdownMenu } from './DropdownMenu';
 export { default as InfiniteList } from './InfiniteList';
 export { default as Loader } from './Loader';
@@ -8,5 +11,5 @@ export { default as MenuLink } from './MenuLink';
 export { ArrowLeft, ArrowRight } from './Navigation';
 export { default as NavLink } from './NavLink';
 export { default as Popover } from './Popover';
-export { default as Slider} from './Slider'
+export { default as Slider } from './Slider';
 export { default as TemporaryDrawer } from './TemporaryDrawer';
