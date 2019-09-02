ac2a7c1612ea363f3bd70243ca79dd828b71a1f2
Daniel Ha <danielha@nutranext.net>
2019-09-02 11:12:11 -0700

    Remove console debugging

--- src/pages/gallery/Products.js
+++ src/pages/gallery/Products.js
@@ -10,11 +10,9 @@ import { getGallery } from '../../hooks';
 
 const Products = () => {
   const { products, variants, prices } = useContext(GalleryContext);
-  console.log('Products', products, variants, prices)
   if (!products)
     return null;
   const [ productSlugs, productMap, variantSlugs, variantMap ] = getGallery(products, variants, prices);
-  console.log('Products', productSlugs, productMap, variantSlugs, variantMap)
   return (
     <Container>
       <Grid container spacing={4}>
