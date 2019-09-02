98df80e52944906094e232f23a35c762f58fe24a
Daniel Ha <danielha@nutranext.net>
2019-09-02 16:21:34 -0700

    Remove comments

--- src/hooks/getGallery.js
+++ src/hooks/getGallery.js
@@ -1,10 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-// import GalleryContext from '../contexts/GalleryContext';
 import { getProducts, getVariants, getPrices } from '../utils/product';
 
 export const getGallery = (products, variants, prices) => {
-  // const { products, variants, prices} = useContext(GalleryContext);
   const pricesMap = getPrices(prices);
   const [variantSlugs, variantMap] = getVariants(products, variants, pricesMap);
   const [productSlugs, productMap] = getProducts(products, variants);
