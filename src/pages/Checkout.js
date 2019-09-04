2df94d1989a25cd5e66e6329eaf7681f1ea1fa7a
Leonardo Kim <luckkim87729@hotmail.com>
2019-09-04 08:11:43 -0700

    DC-111: Fix dependencies

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -25,7 +25,7 @@ import {
   ShippingAddressForm,
   BillingAddressForm,
   PaymentForm,
-  ShippingForm,
+  ShippingMethodForm,
   ReviewForm,
   ResultForm
 } from '../components/forms';
@@ -148,7 +148,7 @@ const Checkout = ({ currentUser }) => {
       case 0:
         return <CreateAccountForm onSubmit={handleNext} />;
       case 1:
-        return <ShippingForm onBack={handleBack} onSubmit={handleNext} />;
+        return <ShippingMethodForm onBack={handleBack} onSubmit={handleNext} />;
       case 2:
         return (
           <ShippingAddressForm onBack={handleBack} onSubmit={handleNext} />
