00f754080b0a1e142ca63620135b80505515b894
Kim T <kim200@kims-mbp.attlocal.net>
2019-09-06 09:23:01 -0700

    DC235 - Refactor Login Error Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,20 @@
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Login from '../components/Login';
 import { requestLoginAttempt } from '../modules/account/actions';
+import { statement } from '@babel/template';
 
 const mapDispatchToProps = { requestLoginAttempt };
 
+Login.propTypes = {
+  account: PropTypes.object
+};
+
+const mapStateToProps = state => ({
+  account: state.account
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Login);
