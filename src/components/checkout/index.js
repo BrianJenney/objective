6f4b16cfdff2626fcf46db1d227e14b5431b81a7
Daniel Ha <danielha@nutranext.net>
2019-09-10 17:18:33 -0700

    Remove ReduxForm as last step in checkout process. When clicking on Place Order button in checkout process, a order confirmation will be presented to the user on /order route

--- src/components/checkout/index.js
+++ src/components/checkout/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
 import { get } from 'lodash';
 import { useSnackbar } from 'notistack';
 import { Container, Box, CssBaseline } from '@material-ui/core';
@@ -12,6 +14,7 @@ import { STEPS, STEP_KEYS, DATA_KEYS, shippingMethods } from './constants';
 import { getDefaultEntity } from './helpers';
 
 const Checkout = ({
+  history,
   currentUser,
   cart,
   requestCreateAccount,
@@ -98,6 +101,16 @@ const Checkout = ({
       result = handleReviewStep();
     }
 
+      requestCreateOrder(
+        { ...cart, ...payload, account_jwt },
+        { creditCardToken }
+      );
+      history.push('/order');
+    }
+
+    setActiveStep(activeStep + 1);
+
+    return true;
     if (result) {
       setActiveStep(activeStep + 1);
     }
@@ -159,9 +172,6 @@ const Checkout = ({
             onSubmit={handleNext}
           />
         </Panel>
-        <Panel title={STEPS[6]} collapsible expanded={activeStep === 6}>
-          <ResultForm onSubmit={() => null} />
-        </Panel>
       </Box>
     </Container>
   );
@@ -176,4 +186,4 @@ Checkout.propTypes = {
   requestCreateOrder: PropTypes.func.isRequired
 };
 
-export default Checkout;
+export default withRouter(Checkout);
