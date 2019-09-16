dda1f521a8245c129af355fb33642edcd379fa18
Kevin <kevinch@nutranext.net>
2019-09-16 09:55:32 -0400

    Adding get op to order module

--- src/modules/order/responses.js
+++ src/modules/order/responses.js
@@ -1,5 +1,5 @@
 import store from '../../store';
-import { receivedCreateOrder } from './actions';
+import { receivedCreateOrder, receivedGetOrder } from './actions';
 import { receivedFindOrdersByAccount } from '../account/actions';
 import { requestCreateCart } from '../cart/actions';
 
@@ -35,6 +35,15 @@ export const handleOrderResponse = (status, data, fields, properties) => {
       console.log(properties);
       // This is in the *account* module, so state updates are made in that reducer.
       store.dispatch(receivedFindOrdersByAccount(data.data));
+      break;
+    case 'order.request.get':
+      console.log('****************** Order Get Response ******************');
+      console.log(status);
+      console.log(data);
+      console.log(fields);
+      console.log(properties);
+      // This is in the *account* module, so state updates are made in that reducer.
+      store.dispatch(receivedGetOrder(data.data));
       break;
     default:
       console.log('bad response ' + fields.routingKey);
