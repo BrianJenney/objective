4cbc9829d852d7a467aae1806b8e52f524a52e84
Daniel Ha <danielha@nutranext.net>
2019-09-16 16:45:27 -0700

    Show link for each order in Your Orders

--- src/pages/account/Orders.js
+++ src/pages/account/Orders.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import Link from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
+//import Link from '@material-ui/core/Link';
 
 import { useDispatch } from 'react-redux';
 
