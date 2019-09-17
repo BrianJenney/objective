c9cc9d74769d1617938dc6d950211db5da7812a0
Davaughn White <davaughnw@usmdbthdt-0ml2.npi.com>
2019-09-17 14:24:11 -0400

    Display products based on Categories on Gallery

--- src/pages/gallery/Products.js
+++ src/pages/gallery/Products.js
@@ -1,31 +1,62 @@
 import React, { useContext } from 'react';
-
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-
 import GalleryContext from '../../contexts/GalleryContext';
-
-import ProductSummary from './ProductSummary';
+import CategorySummary from './CategorySummary';
 import { getGallery } from '../../hooks';
 
 const Products = () => {
   const { products, variants, prices } = useContext(GalleryContext);
   if (!products) return null;
-  const [productSlugs, productMap, variantSlugs, variantMap] = getGallery(
+  const [productSlugs, productMap, variantSlugs, variantMap, productCategories] = getGallery(
     products,
     variants,
     prices
   );
+
+
+  const productCategoriesToProducts = {};
+  productCategories.map(productCategory => {
+    products
+      .filter(product => {
+        return product.category === productCategory;
+      })
+      .map(product => {
+        product = productMap.get(product.slug);
+        console.log("product_new",product);
+        if(productCategoriesToProducts[productCategory]){
+          let productCategoryProducts = productCategoriesToProducts[productCategory];
+         
+
+          productCategoriesToProducts[productCategory].push(product);
+
+        }else{
+          productCategoriesToProducts[productCategory] = [product];
+          
+        }
+      });
+  });
+
+ 
   return (
     <Container>
       <Grid container spacing={4}>
-        {productSlugs.map(productSlug => (
-          <ProductSummary
-            key={productSlug}
-            product={productMap.get(productSlug)}
+
+
+        {Object.keys(productCategoriesToProducts).map((key) =>  
+          (<CategorySummary
+            key={key}
+            products={productCategoriesToProducts[key]}
             variantMap={variantMap}
+            category={{title:"Skin & Beauty",description:"Some description",slug:key}}
           />
-        ))}
+          )
+         
+          
+        
+           
+
+          )}
       </Grid>
     </Container>
   );
