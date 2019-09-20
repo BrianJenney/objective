487d82160370b6d1280e7755fb6d1a2fda3d84bf
Rinor Sadiku <rinors@nutranext.net>
2019-09-20 00:01:09 -0400

    LoggedIn/LoggedOut cart functionality

--- src/modules/cart/reducers.js
+++ src/modules/cart/reducers.js
@@ -6,6 +6,8 @@ import {
   REQUEST_PATCH_CART,
   RECEIVED_PATCH_CART,
   SET_CART_DRAWER_OPENED,
+  REQUEST_FETCH_CART_BY_EMAIL,
+  REQUEST_REMOVE_CART_BY_ID
 } from './types';
 
 const localStorageClient = require('store');
@@ -14,27 +16,33 @@ const INITIAL_STATE = {
   items: [],
   subtotal: 0,
   total: 0,
-  cartDrawerOpened: false,
+  cartDrawerOpened: false
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case REQUEST_CREATE_CART:
-    return { ...state };
-  case RECEIVED_CREATE_CART:
-    localStorageClient.set('cartId', action.payload._id);
-    return { ...state, ...action.payload };
-  case REQUEST_FETCH_CART:
-    return { ...state };
-  case RECEIVED_FETCH_CART:
-    return { ...state, ...action.payload };
-  case REQUEST_PATCH_CART:
-    return { ...state };
-  case RECEIVED_PATCH_CART:
-    return { ...state, ...action.payload };
-  case SET_CART_DRAWER_OPENED:
-    return {...state, cartDrawerOpened: action.payload};
-  default:
-    return state;
+    case REQUEST_CREATE_CART:
+      return { ...state };
+    case RECEIVED_CREATE_CART:
+      localStorageClient.set('cartId', action.payload._id);
+      return { ...state, ...action.payload };
+    case REQUEST_FETCH_CART:
+      return { ...state };
+    case RECEIVED_FETCH_CART:
+      if (action.payload !== undefined)
+        localStorageClient.set('cartId', action.payload._id);
+      return { ...state, ...action.payload };
+    case REQUEST_PATCH_CART:
+      return { ...state };
+    case RECEIVED_PATCH_CART:
+      return { ...state, ...action.payload };
+    case SET_CART_DRAWER_OPENED:
+      return { ...state, cartDrawerOpened: action.payload };
+    case REQUEST_FETCH_CART_BY_EMAIL:
+      return { ...state };
+    case REQUEST_REMOVE_CART_BY_ID:
+      return { ...state };
+    default:
+      return state;
   }
 };
