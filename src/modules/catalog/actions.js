7c85e3641b6577f64c98560899aa4e9dc75170b6
joey <joey@jxlab.io>
2019-09-21 20:47:12 -0400

    Added bootstrap ms, removed individual calls to store, catalog, account, cart on startup

--- src/modules/catalog/actions.js
+++ src/modules/catalog/actions.js
@@ -1,26 +1,4 @@
-import { REQUEST_FETCH_CATALOG, RECEIVED_FETCH_CATALOG } from './types';
-
-const msgpack = require('msgpack-lite');
-const ObjectId = require('bson-objectid');
-
-export const requestFetchCatalog = catalogId => async (dispatch, getState) => {
-  const stompClient = getState().stomp.client;
-  const replyTo = getState().stomp.replyTo;
-  let params = {
-    'id': catalogId
-  };
-
-  var obj = JSON.stringify(msgpack.encode(params));
-  stompClient.send('/exchange/catalog/catalog.request.get', {
-    'reply-to': replyTo,
-    'correlation-id': ObjectId()
-  }, obj);
-
-  dispatch({
-    type: REQUEST_FETCH_CATALOG,
-    payload: {}
-  });
-};
+import { RECEIVED_FETCH_CATALOG } from './types';
 
 export const receivedFetchCatalog = catalog => {
   return {
