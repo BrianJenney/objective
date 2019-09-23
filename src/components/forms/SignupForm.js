a6b7f9d0f91f1284faf02c9f60dad6d2ad1d70c1
Kim T <kim200@kims-mbp.attlocal.net>
2019-09-23 12:44:34 -0700

    Show password triggers Singup func fixed

--- src/components/forms/SignupForm.js
+++ src/components/forms/SignupForm.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Typography } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import { Formik, Field, Form } from 'formik';
 import { object, string, boolean } from 'yup';
 import { CheckboxField, InputField } from '../form-fields';
-import { Button, NavLink } from '../common';
+import { Button, NavLink, AlertPanel } from '../common';
+import { makeStyles } from '@material-ui/core/styles';
+import { withCurrentUser } from '../../hoc';
+
+const useStyles = makeStyles(theme => ({
+  text: {
+    fontFamily: 'p22-underground',
+    fontSize: '14px',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '10px'
+    }
+  },
+  subText: {
+    fontFamily: 'p22-underground',
+    fontSize: '11px',
+    textAlign: 'left',
+    margin: theme.spacing(1, 0)
+  }
+}));
 
 const INITIAL_VALUES = {
   firstName: '',
@@ -17,13 +37,27 @@ const INITIAL_VALUES = {
 const schema = object().shape({
   firstName: string().required('First name is required'),
   lastName: string().required('Last name is required'),
-  email: string().required('Email is required'),
-  password: string().required('Password is required'),
+  email: string()
+    .email('Invalid email')
+    .required('Email is required'),
+  password: string()
+    .min(6, 'Password has to be longer than 6 characters!')
+    .required('Password is required'),
   newsletter: boolean()
 });
 
-const SignupForm = ({ title, onSubmit }) => {
-  const renderForm = () => (
+const SignupForm = ({ title, onSubmit, currentUser }) => {
+  const classes = useStyles();
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const togglePasswordVisibility = useCallback(
+    event => {
+      event.preventDefault();
+      setPasswordVisible(!passwordVisible);
+    },
+    [passwordVisible, setPasswordVisible]
+  );
+
+  const renderForm = ({ isValid }) => (
     <Form>
       {title && <Typography variant="h6" gutterBottom children={title} />}
       <Grid container spacing={2}>
@@ -41,39 +75,84 @@ const SignupForm = ({ title, onSubmit }) => {
             autoComplete="email"
           />
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs>
           <Field
             name="password"
             label="Password"
             component={InputField}
-            type="password"
+            type={passwordVisible ? 'text' : 'password'}
+            InputProps={{
+              endAdornment: (
+                <Box width={1} textAlign="right">
+                  <NavLink
+                    style={{ fontFamily: 'P22-underground', fontSize: '12px' }}
+                    component="button"
+                    underline="always"
+                    onClick={event => togglePasswordVisibility(event)}
+                    children={
+                      passwordVisible ? 'HIDE PASSWORD' : 'SHOW PASSWORD'
+                    }
+                  ></NavLink>
+                </Box>
+              )
+            }}
             autoComplete="current-password"
           />
+          <Typography className={classes.subText}>
+            Must be at least 6 characters
+          </Typography>
         </Grid>
         <Grid item xs={12}>
           <Field
             name="newsletter"
+            color="primary"
             label="Subscribe to True Health news"
             component={CheckboxField}
           />
         </Grid>
         <Grid item xs={12}>
-          <Button fullWidth type="submit" children="Create account" />
+          <Button
+            fullWidth
+            type="submit"
+            children="Create account"
+            disabled={!isValid}
+            className="create-account-btn"
+          />
         </Grid>
+        {currentUser.error && (
+          <AlertPanel
+            my={2}
+            p={2}
+            type="error"
+            bgcolor="#ffcdd2"
+            text={currentUser.error}
+            variant="subtitle2"
+          />
+        )}
+        {currentUser.data.errorMessage && (
+          <AlertPanel
+            my={2}
+            p={2}
+            type="error"
+            bgcolor="#ffcdd2"
+            text={currentUser.data.errorMessage}
+            variant="subtitle2"
+          />
+        )}
       </Grid>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Typography component="p">
-            By creating an account you agree to the True Health
-          </Typography>
-          <Typography>
-            <NavLink to="/termsandconditions" underline="always">
-              Terms &amp; Conditions&nbsp;
-            </NavLink>
-            &amp;&nbsp;
-            <NavLink to="/privacypolicy" underline="always">
-              Privacy Policy
-            </NavLink>
+          <Typography className={classes.text}>
+            By creating an account you agree to the Objective Wellness&nbsp;
+            <Typography className={classes.text}>
+              <NavLink to="/termsandconditions" underline="always">
+                Terms &amp; Conditions&nbsp;
+              </NavLink>
+              &amp;&nbsp;
+              <NavLink to="/privacypolicy" underline="always">
+                Privacy Policy
+              </NavLink>
+            </Typography>
           </Typography>
         </Grid>
       </Grid>
@@ -92,7 +171,8 @@ const SignupForm = ({ title, onSubmit }) => {
 
 SignupForm.propTypes = {
   title: PropTypes.string,
-  onSubmit: PropTypes.func.isRequired
+  onSubmit: PropTypes.func.isRequired,
+  currentUser: PropTypes.object.isRequired
 };
 
-export default SignupForm;
+export default withCurrentUser(SignupForm);
