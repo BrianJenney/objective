c9e984f3f46e8a136eacd84cef22b0c0db7f98ea
Kim T <kim200@kims-mbp.attlocal.net>
2019-09-27 18:47:26 -0700

    Remove unused code

--- src/components/forms/PromoCodeForm.js
+++ src/components/forms/PromoCodeForm.js
@@ -3,13 +3,10 @@ import { useSelector } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
 import { object, string } from 'yup';
 import { Formik, Field, Form } from 'formik';
-import { Button, AlertPanel } from '../common';
+import { Button } from '../common';
 import { redeemPromoCode } from '../../apis/Voucherify';
 import { addCoupon } from '../../modules/cart/functions';
-import {
-  StyledPromoCode,
-  StyledPromoCodeInput
-} from '../../pages/cart/StyledComponents';
+import { StyledPromoCode } from '../../pages/cart/StyledComponents';
 import { InputField } from '../form-fields';
 import './forms-styles.scss';
 const schema = object().shape({
@@ -29,6 +26,7 @@ const PromoCodeForm = () => {
       if (response.result == 'SUCCESS') {
         addCoupon(cart, response.voucher);
       } else {
+        //Should rewrite error message so user can easily understand it
         setPromoCodeErr(response.message);
       }
     },
@@ -36,69 +34,57 @@ const PromoCodeForm = () => {
   );
 
   const renderForm = () => (
-    <>
-      <Form className="promo-code-form">
-        <Grid container direction="row" xs={12} justify="space-between">
-          <Grid item xs={12}>
-            <StyledPromoCode
-              style={{ 'font-size': '14px', paddingBottom: '10px' }}
-            >
-              Promo Code
-            </StyledPromoCode>
-          </Grid>
-          <Grid
-            container
-            xs={12}
-            style={{ 'align-items': 'flex-start', height: '42px' }}
+    <Form className="promo-code-form">
+      <Grid container direction="row" xs={12} justify="space-between">
+        <Grid item xs={12}>
+          <StyledPromoCode
+            style={{ 'font-size': '14px', paddingBottom: '10px' }}
           >
-            <Grid item xs={10}>
-              <Field
-                name="promoCode"
-                component={InputField}
-                className="promo-code-input"
-              />
-              {promoCodeErr && (
-                <StyledPromoCode
-                  style={{
-                    'font-size': '14px',
-                    'padding-top': '10px',
-                    color: 'red'
-                  }}
-                >
-                  {promoCodeErr}
-                </StyledPromoCode>
-              )}
-            </Grid>
-
-            <Grid item xs={2} style={{ 'text-align': 'right' }}>
-              <Button
-                type="submit"
-                children="Apply"
+            Promo Code
+          </StyledPromoCode>
+        </Grid>
+        <Grid
+          container
+          xs={12}
+          style={{ 'align-items': 'flex-start', height: '42px' }}
+        >
+          <Grid item xs={10}>
+            <Field
+              name="promoCode"
+              component={InputField}
+              className="promo-code-input"
+            />
+            {promoCodeErr && (
+              <StyledPromoCode
                 style={{
-                  color: '#000000',
-                  backgroundColor: 'transparent',
-                  textDecoration: 'underline',
-                  minWidth: '0',
-                  fontWeight: 'normal',
-                  border: 'none',
-                  paddingTop: '10px'
+                  'font-size': '14px',
+                  'padding-top': '10px',
+                  color: 'red'
                 }}
-              />
-            </Grid>
+              >
+                {promoCodeErr}
+              </StyledPromoCode>
+            )}
+          </Grid>
+
+          <Grid item xs={2} style={{ 'text-align': 'right' }}>
+            <Button
+              type="submit"
+              children="Apply"
+              style={{
+                color: '#000000',
+                backgroundColor: 'transparent',
+                textDecoration: 'underline',
+                minWidth: '0',
+                fontWeight: 'normal',
+                border: 'none',
+                paddingTop: '10px'
+              }}
+            />
           </Grid>
         </Grid>
-      </Form>
-      {/* {promoCodeErr && (
-        <AlertPanel
-          my={2}
-          p={2}
-          type="error"
-          bgcolor="#ffcdd2"
-          text={promoCodeErr}
-          variant="subtitle2"
-        />
-      )} */}
-    </>
+      </Grid>
+    </Form>
   );
 
   return (
