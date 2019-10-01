a7a44c87975fc64eb524ea96f8baf5091124332d
Kevin <kevinch@nutranext.net>
2019-09-30 21:59:13 -0400

    Trying to split out actions

--- src/components/checkout/index.js
+++ src/components/checkout/index.js
@@ -156,9 +156,9 @@ const Checkout = ({
         { paymentMethodToken }
       );
     }
-    dispatch(resetCart());
-    setPayload({});
-    history.replace('/order');
+    // dispatch(resetCart());
+    // setPayload({});
+    // history.replace('/order');
 
     return true;
   };
