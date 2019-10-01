e2e4a794702700de7d78fa86c8efc16f91a44d86
Kevin <kevinch@nutranext.net>
2019-10-01 17:02:11 -0400

    merge conflict during pull

---
+++ src/components/checkout/TransactionMessage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+
+const TransactionMessage = ({ orderError }) => (
+  <Box>
+    {orderError ? (
+      <Box>
+        <div>Oops! Something went wrong.</div>
+        <div>
+          Your credit card failed when we tried to process your payment.
+          <br />
+          Please review your information and submit again.
+        </div>
+      </Box>
+    ) : (
+      <>
+        <div>Your order is processing...</div>
+        <div>Please do not hit the back button while your order processes.</div>
+      </>
+    )}
+  </Box>
+);
+
+export default TransactionMessage;
