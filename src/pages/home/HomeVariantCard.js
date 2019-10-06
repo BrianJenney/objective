1c5cb8e75cbafb5605d94e804db5bda456a6b423
joey <joey@jxlab.io>
2019-10-05 23:42:31 -0400

    Removed unused variable cardColor

--- src/pages/home/HomeVariantCard.js
+++ src/pages/home/HomeVariantCard.js
@@ -25,8 +25,7 @@ export const HomeVariantCard = ({ variant }) => {
     effectivePrice,
     slug,
     variantInfo,
-    assets,
-    cardColor
+    assets
   } = variant;
   const handleAddToCart = useCallback(() => {
     setATCAdded(true);
@@ -44,7 +43,7 @@ export const HomeVariantCard = ({ variant }) => {
 
   return (
     <Grid item xs={12} md={4}>
-      <Card className={`tile ${cardColor}`} ref={ref}>
+      <Card className="tile" ref={ref}>
         <NavLink to={`/products/${slug}`}>
           <CardMedia
             style={{ height: 430, width: '100%' }}
