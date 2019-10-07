77d5169c14256930cd323b5f1d3ab4bc7ed16125
joey <joey@jxlab.io>
2019-10-07 14:36:18 -0400

    Fixed tax formatting

--- src/utils/order/OrderUtils.js
+++ src/utils/order/OrderUtils.js
@@ -2,7 +2,13 @@ export const roundTo2Decimal = (number) => Math.round(number * 100) / 100;
 
 export const getOrderTotalSummary = order => {
   const { subtotal, tax, discount } = order;
+
+  if (tax === null || !isNaN(tax)) {
+    tax = 0;
+  }
+
   const total = subtotal - roundTo2Decimal(discount) + roundTo2Decimal(tax);
+
   return {
     subtotal: subtotal.toFixed(2),
     tax: tax.toFixed(2),
