e27d78bdbf39bdb39eb5205aabbae7eab4a4834d
Zaneh Williams <zanehw@nutranext.net>
2019-10-17 16:40:15 -0400

    Attempting to scroll to tabsection

--- src/pages/product/TabSection.js
+++ src/pages/product/TabSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { makeStyles, useTheme } from '@material-ui/core/styles';
@@ -9,13 +9,13 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import { scrollToRef } from '../../utils/misc';
 import ProductContext from '../../contexts/ProductContext';
 
 import './cards-styles.css';
 
 import HowItWorksTab from './HowItWorksTab';
 import SupplementFactsTab from './SupplementFactsTab';
-
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -46,7 +46,7 @@ function a11yProps(index) {
   };
 }
 
-const useStyles = makeStyles( theme => ({
+const useStyles = makeStyles(theme => ({
   indicator: {
     backgroundColor: '#fff'
   },
@@ -76,64 +76,71 @@ const useStyles = makeStyles( theme => ({
 export default function PdpTabs() {
   const { product } = useContext(ProductContext);
   const boxBorder = `solid 2px ${product ? product.color : ''}`;
-  const classes = useStyles({border: boxBorder});
+  const classes = useStyles({ border: boxBorder });
   const theme = useTheme();
   const [value, setValue] = useState(0);
+  const zanehRef = useRef(null);
 
   function handleChange(event, newValue) {
     setValue(newValue);
   }
+  useEffect(() => {
+    if (window.location.href.includes('#')) {
+      scrollToRef(zanehRef);
+    }
+  }, []);
 
   if (!product) return null;
 
-
   return (
-    <Container>
-      <Grid spacing={0} xs={12}>
-        <Box className={classes.wrapper} >
-          <div className={classes.root}>
-            <AppBar position="static" color="white" elevation={0}>
-              <Tabs
+    <div ref={zanehRef}>
+      <Container className="tabSection">
+        <Grid spacing={0} xs={12}>
+          <Box className={classes.wrapper}>
+            <div className={classes.root}>
+              <AppBar position="static" color="white" elevation={0}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  variant="fullWidth"
+                  textColor="primary"
+                  classes={{ indicator: classes.indicator }}
+                >
+                  <Tab
+                    variant="smallHeader"
+                    label="How it works"
+                    {...a11yProps(0)}
+                    className={classes.tabs}
+                    style={{ borderLeft: 'none' }}
+                  />
+                  <Tab
+                    variant="smallHeader"
+                    label="Supplement Facts"
+                    {...a11yProps(1)}
+                    className={classes.tabs}
+                  />
+                </Tabs>
+              </AppBar>
+              <TabPanel
                 value={value}
-                onChange={handleChange}
-                variant="fullWidth"
-                textColor="primary"
-                classes={{ indicator: classes.indicator }}
+                index={0}
+                dir={theme.direction}
+                className="tabpanel"
               >
-                <Tab
-                  variant="smallHeader"
-                  label="How it works"
-                  {...a11yProps(0)}
-                  className={classes.tabs}
-                  style={{ borderLeft: 'none' }}
-                />
-                <Tab
-                  variant="smallHeader"
-                  label="Supplement Facts"
-                  {...a11yProps(1)}
-                  className={classes.tabs}
-                />
-              </Tabs>
-            </AppBar>
-            <TabPanel
-              value={value}
-              index={0}
-              dir={theme.direction}
-              className="tabpanel"
-            >
-              <HowItWorksTab />
-            </TabPanel>
-            <TabPanel
-              value={value}
-              index={1}
-              dir={theme.direction}
-              className="tabpanel"
-            >
-              <SupplementFactsTab />
-            </TabPanel>
-          </div>
-        </Box>
-      </Grid>
-    </Container>
+                <HowItWorksTab />
+              </TabPanel>
+              <TabPanel
+                value={value}
+                index={1}
+                dir={theme.direction}
+                className="tabpanel"
+              >
+                <SupplementFactsTab />
+              </TabPanel>
+            </div>
+          </Box>
+        </Grid>
+      </Container>
+    </div>
   );
 }
