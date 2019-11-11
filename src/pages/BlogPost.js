adc54729a5ca9accc31dd392316e612751d97fc1
Kim T <kim200@kims-mbp.attlocal.net>
2019-11-11 11:14:13 -0800

    Remove props on Container

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -200,7 +200,7 @@ const BlogPost = ({ computedMatch }) => {
           {post.fields.relatedProducts &&
           post.fields.relatedProducts.length > 0 ? (
               <Box className="shop">
-                <Container style={{ marginLeft: 15 }}>
+                <Container>
                   <h1 className="title" align="center">
                   SHOP THIS POST
                 </h1>
