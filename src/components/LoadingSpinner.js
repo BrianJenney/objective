218700d1042dfed8b9dcad156560efffa9652c70
Kim T <kim200@kims-mbp.attlocal.net>
2019-11-15 09:37:09 -0800

    Fixed footer-flickering for HomePage

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -18,7 +18,7 @@ const useStyles = makeStyles(theme => ({
 const LoadingSpinner = ({ location, loadingMessage, page }) => {
   const classes = useStyles();
   let size = 0;
-  if (page === 'pdp') size = 45;
+  if (page === 'pdp' || page === 'home') size = 45;
   if (page === 'gallery') size = 10;
   if (page === 'journal') size = 100;
 
