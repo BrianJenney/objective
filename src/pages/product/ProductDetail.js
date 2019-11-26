5a2c72ecb599974f08dce7054c1e57c619b07737
Kim T <kim200@kims-mbp.attlocal.net>
2019-11-26 08:27:03 -0800

    Update condition to display state restriction dialog

--- src/pages/product/ProductDetail.js
+++ src/pages/product/ProductDetail.js
@@ -305,7 +305,7 @@ const ProductDetail = () => {
                       )}
                     </Grid>
                   )}
-                  {content.productTitle === 'Relief + Cooling' && (
+                  {variant.restrictions.definitions.length && (
                     <ShippingRestrictionMobile
                       onClick={handleShippingRestrictions}
                     />
