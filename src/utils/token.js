f2d9e2cab3e496f3231cf5b35e46def7669ac051
joey <joey@jxlab.io>
2019-12-08 19:52:57 -0500

    Removed JWT secret from front-end

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,24 +1,5 @@
-const jwt = require('jsonwebtoken');
-
+// Manually Generated (should move this to SSR)
+// Each Store should have it's own JWT secret set in the store
 export const createAnonymousToken = () => {
-  const token = jwt.sign({
-    store_code: process.env.REACT_APP_STORE_CODE,
-    account_type: 'anonymous'
-  }, process.env.REACT_APP_JWT_SECRET);
-
-  return token;
-};
-
-export const validateToken = token => {
-  jwt.verify(token, process.env.REACT_APP_JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return false;
-    }
-
-    if (decoded) {
-      return decoded;
-    }
-  });
-
-  return false;
+  return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdG9yZV9jb2RlIjoiT0JKIiwiYWNjb3VudF90eXBlIjoiYW5vbnltb3VzIiwiaWF0IjoxNTE2MjM5MDIyfQ.8xFOUpz1QTAv7bxRQWXF0JLwtTYBtnIzUHmzHHgvwzw';
 };
