cd9a68b19e180cb39cb27ad5292c1e8d84d32289
Daniel Ha <danielha@nutranext.net>
2019-12-26 12:42:08 -0800

    Add requestFetchStorefrontSeo action

--- src/modules/storefront/actions.js
+++ src/modules/storefront/actions.js
@@ -1,8 +1,21 @@
-import { RECEIVED_FETCH_STOREFRONT } from './types';
+import { RECEIVED_FETCH_STOREFRONT, REQUEST_FETCH_STOREFRONT_SEO, RECIEVED_FETCH_STOREFRONT_SEO } from './types';
+import { REQUEST_CREATE_ACCOUNT } from '../account/types';
+import { fetchStorefrontSeo } from '../../utils/blog';
 
 export const receivedFetchStorefront = storeFront => {
   return {
     type: RECEIVED_FETCH_STOREFRONT,
     payload: storeFront
   };
+};
+
+export const requestFetchStorefrontSeo = () => (dispatch) => {
+  dispatch({
+    type: REQUEST_FETCH_STOREFRONT_SEO,
+  });
+  const seoMap = fetchStorefrontSeo();
+  dispatch({
+    type: RECIEVED_FETCH_STOREFRONT_SEO,
+    payload: seoMap
+  });
 };
