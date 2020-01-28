c698f117fa331d424359cd3d02152c5bc5fdae25
Emily <emilyk@nutranext.net>
2020-01-27 19:20:56 -0500

    temp - use contentful preview homepage id

--- src/constants/contentfulEntries.js
+++ src/constants/contentfulEntries.js
@@ -1,1 +1,2 @@
-export const OBJECTIVE_HOMEPAGE = '3H6iwfYo7Wk8tS9JT4wFWA';
+// export const OBJECTIVE_HOMEPAGE = '3H6iwfYo7Wk8tS9JT4wFWA';
+export const OBJECTIVE_HOMEPAGE = '5ZMEOgHLRV4eLcOgutw0l4';
