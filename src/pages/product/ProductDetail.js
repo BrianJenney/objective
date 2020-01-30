f12a8ef0591f7c5b7845ae4b1d002cc6d91e07d1
Leonardo Kim <luckkim87729@hotmail.com>
2020-01-30 06:02:22 -0800

    DC-906: Update pdp design for desktop

--- src/pages/product/ProductDetail.js
+++ src/pages/product/ProductDetail.js
@@ -1,4 +1,10 @@
-import React, { useState, useContext, useCallback, useEffect } from 'react';
+import React, {
+  useState,
+  useContext,
+  useCallback,
+  useEffect,
+  useRef
+} from 'react';
 import { withRouter } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -10,9 +16,12 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import { BLOCKS } from '@contentful/rich-text-types';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import ProductContext from '../../contexts/ProductContext';
 import { useQuantity, useWindowSize } from '../../hooks';
+import { Panel } from '../../components/common';
 import Carousel from '../../components/ProductSlider/PDPSlider';
 import './overrides.css';
 import { addToCart } from '../../modules/cart/functions';
@@ -29,12 +38,16 @@ import {
   ShippingRestriction
 } from './ShippingRestrictions';
 import ShippingRestrictionsDialog from './ShippingRestrictionsDialog';
-import './PDP-style.css';
+import './PDP-style.scss';
 import LoadingSpinner from '../../components/LoadingSpinner';
+
+const plusIcon = require('../../assets/images/plus_symbol.svg');
+const closeIcon = require('../../assets/images/close_symbol.svg');
 
 const useStyles = makeStyles(theme => ({
   maxWidth: {
-    maxWidth: '464px'
+    maxWidth: '464px',
+    justifyContent: 'center'
   },
   cardRootOverrides: {
     display: 'flex',
@@ -62,6 +75,25 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const contentfulOptions = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: node => {
+      let params = '?w=555&fm=jpg&q=50';
+
+      if (window.screen.width < 768) {
+        params = '?w=450&fm=jpg&q=50';
+      }
+
+      return (
+        <img
+          src={node.data.target.fields.file.url + params}
+          alt={node.data.target.fields.title}
+        />
+      );
+    }
+  }
+};
+
 let analyticsTracked = false;
 const ProductVariant = ({ productVariant }) => {
   if (!analyticsTracked) {
@@ -85,8 +117,6 @@ const ProductVariant = ({ productVariant }) => {
       alignItems="flex-start"
       className="pdp-product-variant"
     >
-      <div className="pdp-price">${productVariant.effectivePrice}</div>
-      <div className="pdp-price-dash">&mdash;</div>
       <div className="pdp-price-description">
         {productVariant.variantInfo.size} {productVariant.variantInfo.prodType}
       </div>
@@ -94,9 +124,138 @@ const ProductVariant = ({ productVariant }) => {
   ) : null;
 };
 
+const ProductAccordion = ({ content }) => {
+  const accordionItems = [
+    {
+      title: 'Clinical Results',
+      className: 'clinical-results',
+      content: documentToReactComponents(
+        content.clinicalResults,
+        contentfulOptions
+      )
+    },
+    {
+      title: 'Ingredients',
+      className: 'ingredients',
+      content: documentToReactComponents(content.ingredients, contentfulOptions)
+    },
+    {
+      title: 'Directions',
+      className: 'directions',
+      content: (
+        <>
+          <div className="summary">{content.directions.summary}</div>
+          <div className="details">
+            <div className="entry">
+              <div className="icon">
+                <img src={content.directions.details[0].icon} alt="" />
+              </div>
+              <div className="text">{content.directions.details[0].label}</div>
+            </div>
+            <div className="entry">
+              <div className="icon">
+                <img src={content.directions.details[1].icon} alt="" />
+              </div>
+              <div className="text">{content.directions.details[1].label}</div>
+            </div>
+          </div>
+        </>
+      )
+    },
+    {
+      title: 'Frequently Asked Questions',
+      className: 'faqs',
+      content: documentToReactComponents(
+        content.frequentlyAskedQuestions,
+        contentfulOptions
+      )
+    },
+    {
+      title: 'Supplement Facts',
+      className: 'supplement-facts',
+      content: (
+        <>
+          <div className="ingredients">
+            {content.supplementFactsIngredientsParagraph ? (
+              <div className="ingredients-pg">
+                {content.supplementFactsIngredientsParagraph}
+              </div>
+            ) : (
+              <table cellPadding="0" cellSpacing="0">
+                <thead>
+                  <tr>
+                    <th>Each serving contains</th>
+                    <th width="87px">Amount</th>
+                    <th width="90px">% Daily Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {content.supplementFactsIngredients.map(
+                    (ingredient, index) => (
+                      <tr key={`tr_${index.toString()}`}>
+                        <td>{ingredient.ingredient}</td>
+                        <td>{ingredient.amount}</td>
+                        <td>{ingredient.dailyValue}</td>
+                      </tr>
+                    )
+                  )}
+                </tbody>
+              </table>
+            )}
+          </div>
+          <div className="notes">
+            {documentToReactComponents(
+              content.supplementFactsNotes,
+              contentfulOptions
+            )}
+          </div>
+          <div className="other-ingredients">
+            {documentToReactComponents(
+              content.supplementFactsOtherIngredients1,
+              contentfulOptions
+            )}
+          </div>
+          <div className="important">
+            {documentToReactComponents(
+              content.supplementFactsImportant1,
+              contentfulOptions
+            )}
+          </div>
+        </>
+      )
+    }
+  ];
+
+  return (
+    <>
+      {accordionItems.map((accordionItem, index) => (
+        <Panel
+          key={`accordion_item_${index.toString()}`}
+          title={
+            <div className="expansion-panel-title">
+              <div className="title-text">{accordionItem.title}</div>
+              <div className="plus-icon">
+                <img src={plusIcon} alt="" />
+              </div>
+              <div className="close-icon">
+                <img src={closeIcon} alt="" />
+              </div>
+            </div>
+          }
+          collapsible
+          hideExpandIcon
+        >
+          <div className={accordionItem.className}>{accordionItem.content}</div>
+        </Panel>
+      ))}
+    </>
+  );
+};
+
 const ProductDetail = () => {
   const classes = useStyles();
   const cart = useSelector(state => state.cart);
+  const atcRef = useRef();
   const dispatch = useDispatch();
   const { product, variants, prices, content } = useContext(ProductContext);
   // const { enqueueSnackbar } = useSnackbar();
@@ -116,7 +275,16 @@ const ProductDetail = () => {
   const [seconds, setSeconds] = useState(0);
   const [prodLoaded, setProdLoaded] = useState(false);
   // const message = (<ATCSnackbarAction variant={variantMap.get(selectedVariantSku)} />);
-
+  const handleWindowScroll = evt => {
+    const { scrollTop } = evt.target.scrollingElement;
+    if (atcRef.current) {
+      if (scrollTop > 530) {
+        atcRef.current.style.position = 'static';
+      } else {
+        atcRef.current.style.position = 'fixed';
+      }
+    }
+  };
   const updateQuantityToCart = useCallback(
     qty => {
       if (selectedVariantSku === null) return;
@@ -175,6 +343,13 @@ const ProductDetail = () => {
   const closeShippingRestrictions = useCallback(() => {
     setOpenShippingRestrictions(false);
   }, [setOpenShippingRestrictions]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleWindowScroll);
+    return () => {
+      window.removeEventListener('scroll', handleWindowScroll);
+    };
+  }, []);
 
   useEffect(() => {
     setSelectedVariantSku(defaultSku);
@@ -276,6 +451,9 @@ const ProductDetail = () => {
                     <Grid className="mobile-padding-small">
                       <CardActions className={classes.maxWidth}>
                         <ATC
+                          price={
+                            variantMap.get(selectedVariantSku).effectivePrice
+                          }
                           maxWidth={classes.maxWidth}
                           onClick={handleAddToCart}
                           variantSku={selectedVariantSku}
@@ -330,57 +508,66 @@ const ProductDetail = () => {
         <div className={classes.gridModifications}>
           <Container>
             <Grid container xs={12} sm={12}>
-              <Grid container spacing={5} xs={12} sm={12}>
-                <Grid item xs={12} sm={6}>
+              <Grid container xs={12} sm={12}>
+                <Grid item xs={12} sm={8}>
                   <Carousel images={content.productImages} />
                 </Grid>
-                <Grid item xs={12} sm={6}>
+                <Grid item xs={12} sm={4}>
                   <Card className={classes.box}>
                     <CardContent
                       className={classes.cardRootOverrides}
                       className="pdp-content"
                     >
-                      <h1
-                        className="pdp-header"
-                        style={{ color: product.color }}
-                      >
-                        {content.productTitle}
-                      </h1>
+                      <h1 className="pdp-header">{content.productTitle}</h1>
                       <ProductVariant
                         productVariant={variantMap.get(selectedVariantSku)}
                       />
                       <div className="pdp-subtitle">
                         {content.shortPurposeHeadline}
                       </div>
-                      <Typography className="pdp-description">
+                      <div className="pdp-description">
                         {content.shortDescription}
-                      </Typography>
-                      <Typography className="pdp-direction">
-                        DIRECTIONS
-                      </Typography>
-                      <Typography className="pdp-direction-description">
-                        {content.shortDirections}
-                      </Typography>
-
-                      {/* <ProductVariantType
-                  isMobile={isMobile}
-                  variantSlug={variantSlug}
-                  updateTerminalVariant={updateTerminalVariant}
-                /> */}
+                      </div>
+                      <div className="pdp-benefits">
+                        {content.productBenefits.map((benefit, index) => (
+                          <div className="benefit" key={index.toString()}>
+                            <div className="icon">
+                              <img
+                                src={benefit.fields.icon.fields.file.url}
+                                alt=""
+                              />
+                            </div>
+                            <div className="text">
+                              <span>{benefit.fields.benefitText}</span>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                      <div className="pdp-accordion">
+                        <ProductAccordion content={content} />
+                      </div>
                       {!ATCEnabled && <Quantity />}
                     </CardContent>
                     {ATCEnabled && variant.inventory.quantityInStock >= 200 && (
-                      <Grid>
-                        <CardActions className={classes.maxWidth}>
+                      <CardActions className={classes.maxWidth}>
+                        <div className="pdp-atc-container" ref={atcRef}>
                           <ATC
+                            price={
+                              variantMap.get(selectedVariantSku).effectivePrice
+                            }
                             maxWidth={classes.maxWidth}
                             onClick={handleAddToCart}
                             variantSku={selectedVariantSku}
                             ATCAdded={ATCAdded}
                             ATCAdding={ATCAdding}
                           />
-                        </CardActions>
-                      </Grid>
+                          {variant.restrictions && (
+                            <ShippingRestriction
+                              onClick={handleShippingRestrictions}
+                            />
+                          )}
+                        </div>
+                      </CardActions>
                     )}
                     {variant.inventory.quantityInStock < 200 && (
                       <Grid>
@@ -414,11 +601,6 @@ const ProductDetail = () => {
                           />
                         )}
                       </Grid>
-                    )}
-                    {variant.restrictions && (
-                      <ShippingRestriction
-                        onClick={handleShippingRestrictions}
-                      />
                     )}
                     {openShippingRestrictions && (
                       <ShippingRestrictionsDialog
