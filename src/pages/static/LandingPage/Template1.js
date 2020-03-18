16fb12e35bbf536cc6ecf4f75c00661ea8193cc5
Emily <emilyk@nutranext.net>
2020-03-18 15:59:37 -0400

    pass mock data and render header component

--- src/pages/static/LandingPage/Template1.js
+++ src/pages/static/LandingPage/Template1.js
@@ -1,30 +1,23 @@
 import React from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { useTheme } from '@material-ui/core/styles';
 import { useMediaQuery, Container, Grid } from '@material-ui/core';
-import Logo from '../../../components/common/Icons/Logo/Logo';
 import '../../landingpages/fast-asleep.scss';
 
+import mockData from '../sample_data';
+import Header from './Header.js';
+
 const Template1 = ({ match }) => {
-	// const { landingSlug } = match.params;
-	const theme = useTheme();
-	const mobile = useMediaQuery(theme.breakpoints.down('xs'));
+  const mockComponents = mockData.sampleContentful.components;
+  // const { landingSlug } = match.params;
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down('xs'));
 
-	return (
-		<div className="fast-asleep-lp">
-			<div className="landing-header">
-				<Container>
-					<div className="link-holder">
-						<NavLink to="/">
-							<Logo />
-						</NavLink>
-					</div>
-				</Container>
-
-				<Container></Container>
-			</div>
-		</div>
-	);
+  return (
+    <div className="fast-asleep-lp">
+      <Header data={mockComponents} />
+    </div>
+  );
 };
 
 export default withRouter(Template1);
