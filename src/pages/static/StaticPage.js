c7e19e617857d17e041a5837edff60426a6690f7
Kim T <kim200@Kims-MacBook-Pro.local>
2020-03-18 21:10:26 -0700

    Transform metadata

--- src/pages/static/StaticPage.js
+++ src/pages/static/StaticPage.js
@@ -1,12 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Box from '@material-ui/core/Box';
 import { requestPage } from '../../modules/static/actions';
 
-import Box from '@material-ui/core/Box';
-
 const StaticPage = ({ location }) => {
-  //const [featuredMain, setFeaturedMain] = useState({});
+  // const [featuredMain, setFeaturedMain] = useState({});
   const dispatch = useDispatch();
+  const { log } = console;
+  const contentfulEntries = useSelector(state => state.page.items);
+
+  const transformMetadata = metaEntries => {
+    const metaFields = metaEntries.reduce((metaObj, { fields }) => {
+      const metaStyles = Object.keys(fields).filter(each => each !== 'contentType' && each !== 'pageName');
+      const metaData = metaStyles.reduce((obj, each) => {
+        if (!obj[each]) {
+          obj[each] = fields[each];
+        }
+        return obj;
+      }, {});
+
+      for (const [key, value] of Object.entries(fields)) {
+        if (key === 'contentType') {
+          metaObj.type = value;
+        }
+        if (key === 'pageName') {
+          if (value.includes('Desktop')) {
+            metaObj.desktopStyle = { ...metaData };
+          }
+          if (value.includes('Mobile')) {
+            metaObj.mobileStyle = { ...metaData };
+          }
+        }
+      }
+      return metaObj;
+    }, {});
+    return metaFields;
+  };
+  // START
+  if (contentfulEntries) {
+    const object = {};
+    const mainContent = contentfulEntries[0].fields.content.map(entry => {
+      const metaDataSection = transformMetadata(entry.fields.metadata);
+      // log('testing-METADATA', metaDataSection);
+      // const sectionValue = transformData(entry.fields
+    });
+  }
 
   useEffect(() => {
     dispatch(requestPage('staticpage'));
