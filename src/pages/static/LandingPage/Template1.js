4007fa8227bc1100cf594d166b899ed4eeb5fbc8
Emily <emilyk@nutranext.net>
2020-03-19 19:50:43 -0400

    [WIP] add minor styling to desktop / mobile

--- src/pages/static/LandingPage/Template1.js
+++ src/pages/static/LandingPage/Template1.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { useTheme } from '@material-ui/core/styles';
-import { useMediaQuery, Container, Grid } from '@material-ui/core';
-import '../../landingpages/fast-asleep.scss';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Container, Grid } from '@material-ui/core';
 
 import mockData from '../sample_data';
 import Header from '../LandingPage/Header.js';
@@ -11,35 +10,63 @@ import Subtitle from '../Subtitle.js';
 import Hero from '../Hero.js';
 import Paragraph from '../Paragraph';
 import LPBox from '../LandingPage/LPBox.js';
+import Image from '../Image.js';
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    margin: '55px 0 15px',
+    [theme.breakpoints.down('sm')]: {
+      margin: '15px 0 5px'
+    }
+  },
+  subtitle: {
+    margin: '0 0 45px;',
+    [theme.breakpoints.down('sm')]: {
+      margin: '0 0 10px'
+    }
+  },
+  margin: {
+    margin: '0 auto'
+  },
+  image: {
+    marginBottom: 45,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: 25
+    }
+  }
+}));
 
 const Template1 = ({ match }) => {
   const mockComponents = mockData.sampleContentful.components;
   // const { landingSlug } = match.params;
-  const theme = useTheme();
-  const mobile = useMediaQuery(theme.breakpoints.down('xs'));
+  const classes = useStyles();
 
   return (
-    <Grid>
+    <div>
       <Header data={mockComponents} />
       <Container>
-        <Grid xs={12} md={8} container>
-          <Title data={mockComponents} />
-          <Subtitle data={mockComponents} />
+        <Grid item xs={12} md={8} className={classes.margin}>
+          <Grid className={classes.title}>
+            <Title data={mockComponents} />
+          </Grid>
+          <Grid className={classes.subtitle}>
+            <Subtitle data={mockComponents} />
+          </Grid>
         </Grid>
-        <Grid>
+        <Grid item md={10} className={`${classes.margin} ${classes.image}`}>
           <Hero data={mockComponents} />
         </Grid>
-        <Grid className="oneColumn">
+        {/* <Grid item xs={8} className={classes.margin}>
           <Paragraph data={mockComponents[4]} />
           <LPBox data={mockComponents[4]} />
-        </Grid>
-        {/* <Grid className="oneColumn">
-          <Title/>
-          <Paragraph data={mockComponents[6]} />
-          <LPBox data={mockComponents[6]} />
         </Grid> */}
+        <Grid item xs={12} md={8} className={classes.margin}>
+          <Title data={mockComponents[5].value.components} />
+          <Image data={mockComponents[5]} />
+          <Paragraph data={mockComponents[5]} />
+        </Grid>
       </Container>
-    </Grid>
+    </div>
   );
 };
 
