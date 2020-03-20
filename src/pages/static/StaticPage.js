d43be6424143630dd003c79a6ec9549295a9a66f
Kim T <kim200@Kims-MacBook-Pro.local>
2020-03-19 23:30:19 -0700

    Transform Navigation Link

--- src/pages/static/StaticPage.js
+++ src/pages/static/StaticPage.js
@@ -6,8 +6,14 @@ import { requestPage } from '../../modules/static/actions';
 const StaticPage = ({ location }) => {
   // const [featuredMain, setFeaturedMain] = useState({});
   const dispatch = useDispatch();
-  const { log } = console;
   const contentfulEntries = useSelector(state => state.page.items);
+
+  const dataObj = {
+    template: 'Template-1',
+    slug: 'staticpage',
+    components: []
+  };
+  const { log } = console;
 
   const transformMetadata = metaEntries => {
     const metaFields = metaEntries.reduce((metaObj, { fields }) => {
@@ -37,19 +43,44 @@ const StaticPage = ({ location }) => {
     return metaFields;
   };
 
-  const transformData = entries => {
-    const currentEntries = entries;
-    // log('testing+++', currentEntries); // {data:{}. content:[], nodeType: ''}
+  const transformNavLink = (name, store, entries) => {
+    if (!entries.content) {
+    } else {
+      entries.content.map(entry => transformNavLink(name, store, entry));
+    }
+    if (entries.nodeType === 'hyperlink') {
+      store.push({
+        scroll: entries.data.uri,
+        label: entries.content[0].value
+      });
+    }
+    return store;
   };
+
   // START
   if (contentfulEntries) {
-    const object = {};
+    const currentStore = {};
     const mainContent = contentfulEntries[0].fields.content.map(({ fields }) => {
+      // log('testing-fields', fields);
       const metaDataSection = transformMetadata(fields.metadata);
-      // const sectionValue = transformData(fields.content);
-      log('testing', fields);
+      // log('testing-METADATA', metaDataSection);
+      if (metaDataSection.type === 'navigation') {
+        if (fields.name.toLowerCase().includes('mobile')) {
+          const navLinkData = transformNavLink(fields.name, [], fields.content);
+          currentStore.value = navLinkData;
+        }
+        if (fields.name.toLowerCase().includes('desktop')) {
+          const navLinkData = transformNavLink(fields.name, [], fields.content);
+          currentStore.value = navLinkData;
+        }
+        dataObj.components.push({
+          ...currentStore,
+          ...metaDataSection
+        });
+      }
     });
   }
+  // log('TESTING---', dataObj);
 
   useEffect(() => {
     dispatch(requestPage('staticpage'));
