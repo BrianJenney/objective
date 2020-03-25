ed232a6ab45c251e1d733fabc8758f0a0fad8b4e
Zaneh Williams <zanehw@Zaneh-Williams-MBP16-2019.local>
2020-03-25 15:27:05 -0400

    fixed image dimensions

--- src/pages/landingpages/SleepImmunity.js
+++ src/pages/landingpages/SleepImmunity.js
@@ -21,13 +21,13 @@ const SleepImmunity = ({ location }) => {
       <Box className="hero-holder">
         {mobile ? (
           <img
-            src="https://images.ctfassets.net/mj9bpefl6wof/aNIREwk0W5vgpnlOiMOuj/72ac1cf06280fadf0f984b68bbf7342c/Sleep_and_Immune_Bundle_Gallery_Hero_mobile.jpeg?h=570"
+            src="https://images.ctfassets.net/mj9bpefl6wof/aNIREwk0W5vgpnlOiMOuj/72ac1cf06280fadf0f984b68bbf7342c/Sleep_and_Immune_Bundle_Gallery_Hero_mobile.jpeg?w=450"
             alt=""
             className="gallery-hero"
           />
         ) : (
             <img
-              src="https://images.ctfassets.net/mj9bpefl6wof/26iguhDwNRUqALzOH49inO/fdd98e688e149d99f450bb31ff94a69f/Sleep_and_Immune_Bundle_Gallery_Hero_desktop.jpeg?w=450"
+              src="https://images.ctfassets.net/mj9bpefl6wof/26iguhDwNRUqALzOH49inO/fdd98e688e149d99f450bb31ff94a69f/Sleep_and_Immune_Bundle_Gallery_Hero_desktop.jpeg?h=630"
               alt=""
               className="gallery-hero"
             />
