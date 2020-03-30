c8a83fa9bb6d14da77df49f22af13919696f9c3a
Emily <emilyk@nutranext.net>
2020-03-30 13:20:28 -0400

    style mobile image caption

--- src/pages/static/components/Image.js
+++ src/pages/static/components/Image.js
@@ -15,6 +15,10 @@ const useStyles = makeStyles(theme => ({
   }),
   captionImage: {
     marginBottom: 10
+  },
+  captionImageMobile: {
+    width: '100%',
+    marginBottom: 10
   }
 }));
 
@@ -24,7 +28,7 @@ const Image = ({ data, template, type, caption }) => {
   const theme = useTheme();
   const sm = useMediaQuery(theme.breakpoints.down('sm'));
   const float = data.desktopStyle.float || data.mobileStyle.float;
-  console.log(caption);
+
   return (
     <div>
       {!sm ? (
@@ -36,6 +40,11 @@ const Image = ({ data, template, type, caption }) => {
         ) : (
           <img src={data.desktopImg} className={`${classes.root} ${template}-${type}-${float}`}></img>
         )
+      ) : caption ? (
+        <div className={`${template}-${type}-caption-${float}`}>
+          <img src={data.mobileImg} className={classes.captionImageMobile} />
+          <div className={classes.caption}>{caption.value}</div>
+        </div>
       ) : (
         <img src={data.mobileImg} className={`${classes.root} ${template}-${type}-${float}`}></img>
       )}
