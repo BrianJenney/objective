fcc20c54e0a5a96f3ee465106295e69e11bb7c94
Emily <emilyk@nutranext.net>
2020-04-03 11:46:43 -0400

    add default style to mobile header component

--- src/pages/static/components/HeaderMobile.js
+++ src/pages/static/components/HeaderMobile.js
@@ -6,35 +6,61 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import './template-styles.css';
 
 const useStyles = makeStyles(theme => ({
-  // root: {},
-  // link: props => ({
-  //   color: props.desktopStyle.fontColor,
-  //   fontWeight: props.desktopStyle.fontWeight,
-  //   fontSize: props.desktopStyle.fontSize,
-  //   fontFamily: props.desktopStyle.fontFamily,
-  //   lineHeight: props.desktopStyle.lineHeight,
-  //   textTransform: props.desktopStyle.textTransform,
-  //   width: props.desktopStyle.width
-  // })
+  root: {
+    [theme.breakpoints.down('sm')]: {
+      border: '1px solid #000',
+      margin: '15px 0 15px 0',
+      padding: 10
+    }
+  },
+  link: props => ({
+    color: props.mobileStyle.fontColor,
+    fontWeight: props.mobileStyle.fontWeight,
+    fontSize: props.mobileStyle.fontSize,
+    fontFamily: props.mobileStyle.fontFamily,
+    lineHeight: props.mobileStyle.lineHeight,
+    textTransform: props.mobileStyle.textTransform
+  }),
+  ulList: {
+    marginLeft: '-1em'
+  },
+  list: {
+    marginBottom: 14
+  }
 }));
 
 const HeaderMobile = ({ data, template }) => {
-  const classes = useStyles(data);
+  const mobileNav = data.filter(nav => nav.name.toLowerCase().includes('mobile'));
+  const classes = useStyles(mobileNav[0]);
 
   const theme = useTheme();
   const sm = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
-    <div>
-      {sm
-        ? data.map(nav => {
-            return nav.name.toLowerCase().includes('mobile')
-              ? nav.value.map(val => <a href={val.scroll}>{val.label}</a>)
-              : data.length <= 1 && nav.name.toLowerCase().includes('desktop')
-              ? nav.value.map(val => <a href={val.scroll}>{val.label}</a>)
-              : null;
-          })
-        : null}
+    <div className={classes.root}>
+      <ul className={classes.ulList}>
+        {sm
+          ? data.map(nav => {
+              return nav.name.toLowerCase().includes('mobile')
+                ? nav.value.map(val => (
+                    <li className={classes.list}>
+                      <a href={val.scroll} className={`${classes.link} ${template}-${nav.type}`}>
+                        {val.label}
+                      </a>
+                    </li>
+                  ))
+                : data.length <= 1 && nav.name.toLowerCase().includes('desktop')
+                ? nav.value.map(val => (
+                    <li className={classes.list}>
+                      <a href={val.scroll} className={`${template}-${nav.type}`}>
+                        {val.label}
+                      </a>
+                    </li>
+                  ))
+                : null;
+            })
+          : null}
+      </ul>
     </div>
   );
 };
