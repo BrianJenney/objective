143808eb07e90e3fa729575a11b092b1e55d7157
Kim T <kim200@kims-mbp.attlocal.net>
2020-04-14 09:39:44 -0700

    Fix default value

--- src/pages/product/SupplementFactsTab.js
+++ src/pages/product/SupplementFactsTab.js
@@ -89,11 +89,7 @@ export default function SupplementFactsTab() {
 
   return (
     <div className="label-info-wrapper">
-      {content && content.supplementFactsTitle ? (
-        <h2>{content.supplementFactsTitle}</h2>
-      ) : (
-          <h2>Supplement FactsSSSS</h2>
-        )}
+      {content && content.supplementFactsTitle ? <h2>{content.supplementFactsTitle}</h2> : <h2>Supplement Facts</h2>}
       <div className="top-block" style={{ borderTop: border2 }}>
         <div className="flex-desktop">
           <div className="card facts-text">
