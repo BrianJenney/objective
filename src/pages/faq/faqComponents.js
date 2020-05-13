c65712450d89fe7dc41666e21952c2465eb888e9
Kim Tran <kimt@kimtranbp162019.attlocal.net>
2020-05-13 00:44:53 -0700

    Render styled FAQ SectionTitle component

--- src/pages/faq/faqComponents.js
+++ src/pages/faq/faqComponents.js
@@ -1,58 +1,93 @@
 import React from 'react';
-import { useTheme, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { Box } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-  root: props => ({
-    color: props.desktopStyle.fontColor,
-    fontWeight: props.desktopStyle.fontWeight,
-    fontSize: props.desktopStyle.fontSize || 48,
-    lineHeight: props.desktopStyle.lineHeight || 'normal',
-    fontFamily: props.desktopStyle.fontFamily || 'Canela Text Web',
-    textAlign: props.desktopStyle.align || 'center',
-    margin: props.desktopStyle.margin || '0 0 62px',
-    [theme.breakpoints.down('xs')]: {
-      color: props.mobileStyle.fontColor,
-      fontWeight: props.mobileStyle.fontWeight,
-      fontSize: props.mobileStyle.fontSize || 24,
-      lineHeight: props.mobileStyle.lineHeight || 'normal',
-      fontFamily: props.mobileStyle.fontFamily || 'Canela Text Web',
-      textAlign: props.mobileStyle.align || 'center',
-      margin: props.mobileStyle.margin || '0 0 44px'
-    }
-  })
+  title: props => ({
+	
+			color: props.desktopStyle.fontColor,
+			fontWeight: props.desktopStyle.fontWeight || 'normal',
+			fontSize: props.desktopStyle.fontSize || 48,
+			lineHeight: props.desktopStyle.lineHeight || 'normal',
+			fontFamily: props.desktopStyle.fontFamily || 'Canela Text Web',
+			textAlign: props.desktopStyle.align || 'center',
+			margin: props.desktopStyle.margin || '0 0 62px',
+			[theme.breakpoints.down('xs')]: {
+				color: props.mobileStyle.fontColor,
+				fontWeight: props.mobileStyle.fontWeight,
+				fontSize: props.mobileStyle.fontSize || 24,
+				lineHeight: props.mobileStyle.lineHeight || 'normal',
+				fontFamily: props.mobileStyle.fontFamily || 'Canela Text Web',
+				textAlign: props.mobileStyle.align || 'center',
+				margin: props.mobileStyle.margin || '0 0 44px'
+			}
+	
+	}),
+	subTitle: props => ({
+		color: props.desktopStyle.fontColor,
+		fontWeight: props.desktopStyle.fontWeight || 600,
+		fontSize: props.desktopStyle.fontSize || 24,
+		lineHeight: props.desktopStyle.lineHeight || 'normal',
+		fontFamily: props.desktopStyle.fontFamily || 'p22-underground, sans-serif',
+		textAlign: props.desktopStyle.align || 'left',
+		margin: props.desktopStyle.margin || '0 0 30px',
+		[theme.breakpoints.down('xs')]: {
+			color: props.mobileStyle.fontColor,
+			fontWeight: props.mobileStyle.fontWeight || 600,
+			fontSize: props.mobileStyle.fontSize || 18,
+			lineHeight: props.mobileStyle.lineHeight || 'normal',
+			fontFamily: props.mobileStyle.fontFamily || 'p22-underground, sans-serif',
+			textAlign: props.mobileStyle.align || 'left',
+			margin: props.mobileStyle.margin || '0 0 24px'
+		}
+	})
+	
 }));
 
 export const generateComponents = (page) => {
 	let components = [];
-
+	console.log('testing-PAGE', page);
 	page.components.map(comp => {
 		switch(comp.type) {
 			case 'pageTitle':
         components.push(
-          <Title data={comp}/>
-					
+          <Title data={comp} value={comp.value}/>					
         );
-        break;
+				break;
+			case 'sectionTitle':
+				components.push(
+					<SectionTitle data={comp} value={comp.value}/>
+				);
+				break;
 		}
 	});
-	console.log('testing-COMPPPPP----', components)
+	// console.log('testing-COMPPPPP----', components)
 	return components
 
 
 };
 
 
-export const Title = ({data}) => {
+export const Title = ({ data, value }) => {
   const classes = useStyles(data);
 
   return (
     <>
       <Box>
-        <div className={classes.root}>{data.value}</div>
+        <div className={classes.title}>{value}</div>
       </Box>
     </>
+  );
+};
+
+export const SectionTitle = ({ data, value }) => {
+  const classes = useStyles(data);
+
+  return (
+    <Grid>
+      <Box>
+        <div className={classes.subTitle}>{value}</div>
+      </Box>
+    </Grid>
   );
 };
