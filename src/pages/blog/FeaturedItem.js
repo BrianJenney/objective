d76f154b4b7df6cbfb4bb1988f3fae05aad45e20
Emily <emilyk@nutranext.net>
2020-05-15 11:58:45 -0400

    remove image qualit - 50y

--- src/pages/blog/FeaturedItem.js
+++ src/pages/blog/FeaturedItem.js
@@ -6,7 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import { fetchLinkedCategory } from '../../utils/blog';
 import segmentTrackEditorialItemClicked from '../../utils/segmentTrackEditorialItemClicked';
 
-const FeaturedItem = ({post}) => {
+const FeaturedItem = ({ post }) => {
   const [category, setCategory] = useState('General');
   const [slug, setSlug] = useState(null);
   let segmentAnalyticsTracker = segmentTrackEditorialItemClicked;
@@ -14,14 +14,19 @@ const FeaturedItem = ({post}) => {
   let imageUrl = 'http://cdn1.stopagingnow.com/objective/fakeimg.png';
 
   if (p.fields.featuredImage && p.fields.featuredImage.fields) {
-    imageUrl = `${p.fields.featuredImage.fields.file.url}?w=424&fm=jpg&q=50`;
+    imageUrl = `${p.fields.featuredImage.fields.file.url}?w=424&fm=jpg`;
   }
 
   useEffect(() => {
     if (p.fields.categories && p.fields.categories.length > 0 && p.fields.categories[0].fields) {
       setCategory(p.fields.categories[0].fields.title);
       setSlug(p.fields.categories[0].fields.slug);
-    } else if (p.fields.categories && p.fields.categories.length > 0 && p.fields.categories[0].sys && p.fields.categories[0].sys.type === 'Link') {
+    } else if (
+      p.fields.categories &&
+      p.fields.categories.length > 0 &&
+      p.fields.categories[0].sys &&
+      p.fields.categories[0].sys.type === 'Link'
+    ) {
       fetchLinkedCategory(p.fields.categories[0].sys.id).then(cat => {
         if (cat && cat.fields) {
           setCategory(cat.fields.title);
@@ -33,16 +38,40 @@ const FeaturedItem = ({post}) => {
 
   return (
     <Grid item xs={12} md={4}>
-      <Link onClick={()=>{segmentAnalyticsTracker(p)}} to={`/journal/posts/${ p.fields.slug }`}>
-        <img src={ imageUrl } alt={ p.fields.title } />
+      <Link
+        onClick={() => {
+          segmentAnalyticsTracker(p);
+        }}
+        to={`/journal/posts/${p.fields.slug}`}
+      >
+        <img src={imageUrl} alt={p.fields.title} />
       </Link>
       <div className="flex">
-        <span className="categoryName"><Link to={`/journal/category/${ slug }`}>{ category }</Link></span>&mdash;
-        <span className="minRead">{ p.fields.minuteRead } Min Read</span>
+        <span className="categoryName">
+          <Link to={`/journal/category/${slug}`}>{category}</Link>
+        </span>
+        &mdash;
+        <span className="minRead">{p.fields.minuteRead} Min Read</span>
       </div>
-      <h2><Link onClick={()=>{segmentAnalyticsTracker(p)}} to={`/journal/posts/${ p.fields.slug }`}>{ p.fields.title }</Link></h2>
-      <p>{ p.fields.teaser }</p>
-      <Link onClick={()=>{segmentAnalyticsTracker(p, "Read More")}} to={`/journal/posts/${ p.fields.slug }`}>Read More</Link>
+      <h2>
+        <Link
+          onClick={() => {
+            segmentAnalyticsTracker(p);
+          }}
+          to={`/journal/posts/${p.fields.slug}`}
+        >
+          {p.fields.title}
+        </Link>
+      </h2>
+      <p>{p.fields.teaser}</p>
+      <Link
+        onClick={() => {
+          segmentAnalyticsTracker(p, 'Read More');
+        }}
+        to={`/journal/posts/${p.fields.slug}`}
+      >
+        Read More
+      </Link>
     </Grid>
   );
 };
