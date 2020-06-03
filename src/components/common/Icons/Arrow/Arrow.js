cdac09a5f0ee22653e139bbc7b86253c8a745c6e
Emily <emilyk@nutranext.net>
2020-06-02 23:08:19 -0400

    add arrow svg/component

---
+++ src/components/common/Icons/Arrow/Arrow.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { ReactComponent as Arrow } from '../../../../assets/static/arrow.svg';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '50x',
+    height: '18px',
+    [theme.breakpoints.down('xs')]: {
+      width: '44px',
+      height: '14px'
+    }
+  }
+}));
+
+const ArrowIcon = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Arrow className={classes.root} />
+    </>
+  );
+};
+
+export default ArrowIcon;
