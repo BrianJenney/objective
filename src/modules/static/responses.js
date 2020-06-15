4c00ad2c65f2415a3584da68a6b24e2439c7d829
Anne <annes@nutranext.net>
2020-06-15 14:45:14 -0700

    adds promobannere response case

--- src/modules/static/responses.js
+++ src/modules/static/responses.js
@@ -1,5 +1,5 @@
 import store from '../../store';
-import { receivedPage, receivedPrivacy } from './actions';
+import { receivedPage, receivedPrivacy, receivedPromoBanner } from './actions';
 import { debugRabbitResponse } from '../../utils/misc';
 
 export const handlePageResponse = (status, data, fields, properties) => {
@@ -10,7 +10,14 @@ export const handlePageResponse = (status, data, fields, properties) => {
       if (status === 'success') {
         store.dispatch(receivedPage(data));
       } else {
-        console.log('THIS FAILED');
+        throw status;
+      }
+      break;
+    case 'content.request.contentfulPromoBanner':
+      debugRabbitResponse('Contentful Response', status, data, fields, properties);
+      // status handling
+      if (status === 'success') {
+        store.dispatch(receivedPromoBanner(data));
       }
       break;
     case 'content.request.privacy':
@@ -18,11 +25,9 @@ export const handlePageResponse = (status, data, fields, properties) => {
       // status handling
       if (status === 'success') {
         store.dispatch(receivedPrivacy(data));
-      } else {
-        console.log('THIS FAILED');
       }
       break;
     default:
-      console.log('bad response ' + fields.routingKey);
+      break;
   }
 };
