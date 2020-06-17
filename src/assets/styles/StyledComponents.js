a2b1dfa51da5d50c8bdd5f8c59c9d18db0463ad6
Emily <emilyk@nutranext.net>
2020-06-17 14:18:03 -0400

    adjust padding

--- src/assets/styles/StyledComponents.js
+++ src/assets/styles/StyledComponents.js
@@ -4,8 +4,11 @@ import Container from '@material-ui/core/Container';
 export const StyledContainer = withStyles(theme => ({
   root: {
     maxWidth: 1200,
+    padding: 0,
     [theme.breakpoints.down('sm')]: {
-      maxWidth: '100%'
+      maxWidth: '100%',
+      paddingLeft: 16,
+      paddingRight: 16
     }
   }
 }))(Container);
