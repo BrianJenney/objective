1dbd13d65a169928b1e39d41917ed24cdd397a55
Emily <emilyk@nutranext.net>
2020-06-17 16:04:20 -0400

    adjust padding

--- src/assets/styles/StyledComponents.js
+++ src/assets/styles/StyledComponents.js
@@ -5,7 +5,15 @@ export const StyledContainer = withStyles(theme => ({
   root: {
     maxWidth: 1200,
     padding: 0,
+    [theme.breakpoints.down('md')]: {
+      paddingLeft: 32,
+      paddingRight: 32
+    },
     [theme.breakpoints.down('sm')]: {
+      paddingLeft: 24,
+      paddingRight: 24
+    },
+    [theme.breakpoints.down('xs')]: {
       maxWidth: '100%',
       paddingLeft: 16,
       paddingRight: 16
