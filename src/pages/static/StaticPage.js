3677c554eb46cb16656ccdab0eaa93c301337c41
Anne <annes@nutranext.net>
2020-06-19 10:31:57 -0700

    adds subdomain to contentful ms call

--- src/pages/static/StaticPage.js
+++ src/pages/static/StaticPage.js
@@ -1,27 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 import LoadingSpinner from '../../components/LoadingSpinner';
-import Box from '@material-ui/core/Box';
 import { requestPage } from '../../modules/static/actions';
 import { buildPage } from '../../utils/sputils';
-import NotFound from '../notfound/NotFound';
 
 const StaticPage = ({ match }) => {
   const { slug } = match.params;
   const dispatch = useDispatch();
   const [pageLoaded, setPageLoaded] = useState(false);
   const [tracked, setTracked] = useState(false);
-  const [pageError, setPageError] = useState(false);
   const page = useSelector(state => state.page);
 
   useEffect(() => {
-    if (pageLoaded === false) dispatch(requestPage(slug));
+    const [subdomain] = window.location.hostname.split('.');
+    if (pageLoaded === false) dispatch(requestPage(slug, subdomain));
   }, []);
 
   useEffect(() => {
     // check this when we have real content from content ms
-    if (page.hasOwnProperty('template')) {
+    if (page.template) {
       // check for components here.
       setPageLoaded(true);
       if (tracked === false && page.name) {
@@ -29,23 +29,21 @@ const StaticPage = ({ match }) => {
         window.analytics.page(`LP: ${page.name}`);
       }
     }
-    // else {
-    //   setPageError(true);
-    // }
   }, [page]);
 
   let FinalPage = null;
   if (pageLoaded) {
     FinalPage = () => buildPage(page);
   }
-  // if (pageError) {
-  //   FinalPage = () => <NotFound />;
-  // }
 
   if (FinalPage) {
     return <FinalPage />;
   }
   return <LoadingSpinner loadingMessage="...loading" page="lp" />;
+};
+
+StaticPage.propTypes = {
+  match: PropTypes.object.isRequired
 };
 
 export default withRouter(StaticPage);
