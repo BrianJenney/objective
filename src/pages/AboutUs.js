fb5f09d0c6fee7f813df1b9e66e56c7e2d1566b2
Anne <annes@nutranext.net>
2020-06-25 11:32:24 -0700

    fixes linter errors on AboutUs

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import Divider from '@material-ui/core/Divider';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { Button } from '../components/common';
 import ScrollToTop from '../components/common/ScrollToTop';
 import './about/about-styles.scss';
 import { OBJECTIVE_SPACE } from '../constants/contentfulSpaces';
-import { OBJECTIVE_ABOUTUS } from '../constants/contentfulEntries';
-import { receivedLoginFailure } from '../modules/account/actions';
 import HeadTags from '../components/common/HeadTags';
-
 
 const contentful = require('contentful');
 const contentfulClient = contentful.createClient({
@@ -35,10 +31,7 @@ const contentfulOptions = {
       }
 
       return (
-        <img
-          src={node.data.target.fields.file.url + params}
-          alt={node.data.target.fields.title}
-        />
+        <img src={node.data.target.fields.file.url + params} alt={node.data.target.fields.title} />
       );
     },
     [INLINES.HYPERLINK]: (node, children) => (
@@ -62,7 +55,7 @@ const AboutUs = ({ location }) => {
     const response = await contentfulClient.getEntries({
       content_type: 'aboutUs'
     });
-    const entries = response.items.forEach(entry => {
+    response.items.forEach(entry => {
       if (entry.fields) {
         results.push(entry.fields);
       }
@@ -96,24 +89,20 @@ const AboutUs = ({ location }) => {
 
     return images.map(image => (
       <li key={image.sys.id} style={{ listStyleType: 'none' }}>
-        <img src={image.fields.file.url + params} className="hero" />
+        <img src={image.fields.file.url + params} className="hero" alt={image.fields.title} />
       </li>
     ));
   };
 
   const renderWelcomeText = () => {
     if (!contents.welcomeText) return <></>;
-    return contents.welcomeText.content.map(text => (
-      <p>{text.content[0].value}</p>
-    ));
+    return contents.welcomeText.content.map(text => <p>{text.content[0].value}</p>);
   };
 
   const renderAdditionalText = () => {
     if (!contents.additionalText) return <></>;
     const allContents = contents.additionalText.content;
-    const allTexts = allContents.filter(
-      ({ nodeType }) => nodeType !== 'embedded-asset-block'
-    );
+    const allTexts = allContents.filter(({ nodeType }) => nodeType !== 'embedded-asset-block');
     return allTexts.map(eachText => (
       <div>{documentToReactComponents(eachText, contentfulOptions)}</div>
     ));
@@ -122,7 +111,7 @@ const AboutUs = ({ location }) => {
   const renderApproachBlock = () => {
     if (!contents.approachBlock) return <></>;
     const { content } = contents.approachBlock;
-    const title = content[0].content[0].value;
+    const approachBlockTitle = content[0].content[0].value;
     const subTitle = content[1].content[0].value;
     const allBlocks = content
       .slice(2, 5)
@@ -130,7 +119,7 @@ const AboutUs = ({ location }) => {
     return (
       <>
         <h1>
-          {title}
+          {approachBlockTitle}
           <br />
           {subTitle}
         </h1>
@@ -155,7 +144,7 @@ const AboutUs = ({ location }) => {
   const renderSections = () => {
     if (!contents.aboutUsContent) return <></>;
     const { content } = contents.aboutUsContent;
-    const title = content[0].content[0].value;
+    const sectionsTitle = content[0].content[0].value;
     const subTitle1 = content[1].content[0].value;
     const subTitle2 = content[2].content[0].value;
     const sections = content
@@ -170,42 +159,54 @@ const AboutUs = ({ location }) => {
 
     return (
       <>
-        <h3>{title}</h3>
+        <h3>{sectionsTitle}</h3>
         <h1>
           {subTitle1} <br />
           {subTitle2}
         </h1>
         {mobile ? (
           <div className="mobile-only">
-            <img src={allImgs[1][1].data.target.fields.file.url} />
+            <img
+              src={allImgs[1][1].data.target.fields.file.url}
+              alt={allImgs[1][1].data.target.fields.title}
+            />
             {allTexts[0].map((text, key) => (
               <p className={`textNum${key}`}>{text.content[0].value}</p>
             ))}
-            <img src={allImgs[0][1].data.target.fields.file.url} />
+            <img
+              src={allImgs[0][1].data.target.fields.file.url}
+              alt={allImgs[0][1].data.target.fields}
+            />
             {allTexts[1].map((text, key) => (
               <p className={`textNum${key}`}>{text.content[0].value}</p>
             ))}
           </div>
         ) : (
-            <div className="hundred xs-hidden">
-              <div className="left fifty">
-                <Container className="container-left">
-                  {allTexts[0].map((text, key) => (
-                    <p className={`textNum${key}`}>{text.content[0].value}</p>
-                  ))}
-                </Container>
-                <img src={allImgs[0][0].data.target.fields.file.url} />
-              </div>
-              <div className="right fifty">
-                <img src={allImgs[1][0].data.target.fields.file.url} />
-                <Container className="container-right">
-                  {allTexts[1].map((text, key) => (
-                    <p className={`textNum${key}`}>{text.content[0].value}</p>
-                  ))}
-                </Container>
-              </div>
+          <div className="hundred xs-hidden">
+            <div className="left fifty">
+              <Container className="container-left">
+                {allTexts[0].map((text, key) => (
+                  <p className={`textNum${key}`}>{text.content[0].value}</p>
+                ))}
+              </Container>
+              <img
+                src={allImgs[0][0].data.target.fields.file.url}
+                alt={allImgs[0][0].data.target.fields.title}
+              />
             </div>
-          )}
+            <div className="right fifty">
+              <img
+                src={allImgs[1][0].data.target.fields.file.url}
+                alt={allImgs[1][0].data.target.fields.title}
+              />
+              <Container className="container-right">
+                {allTexts[1].map((text, key) => (
+                  <p className={`textNum${key}`}>{text.content[0].value}</p>
+                ))}
+              </Container>
+            </div>
+          </div>
+        )}
       </>
     );
   };
@@ -233,11 +234,9 @@ const AboutUs = ({ location }) => {
             {mobile ? (
               <>
                 <img
-                  src={
-                    contents.additionalText.content[6].data.target.fields.file
-                      .url
-                  }
+                  src={contents.additionalText.content[6].data.target.fields.file.url}
                   className="mobile-img"
+                  alt={contents.additionalText.content[6].data.target.fields.title}
                 ></img>
                 <Container>
                   <Box>
@@ -249,25 +248,25 @@ const AboutUs = ({ location }) => {
                 </Container>
               </>
             ) : (
-                <div
-                  style={{
-                    backgroundImage: `url("${contents.additionalText.content[4].data.target.fields.file.url.replace(
-                      '//images.ctfassets.net/mj9bpefl6wof/',
-                      'https://nutranext.imgix.net/'
-                    )}?q=50&auto=compress,format")`
-                  }}
-                  className="desktop-img"
-                >
-                  <Container>
-                    <Box>
-                      <div className="text">{renderAdditionalText()}</div>
-                    </Box>
-                    <Link to="/gallery" className="buttonlink mobile-only">
-                      Shop Better Health
+              <div
+                style={{
+                  backgroundImage: `url("${contents.additionalText.content[4].data.target.fields.file.url.replace(
+                    '//images.ctfassets.net/mj9bpefl6wof/',
+                    'https://nutranext.imgix.net/'
+                  )}?q=50&auto=compress,format")`
+                }}
+                className="desktop-img"
+              >
+                <Container>
+                  <Box>
+                    <div className="text">{renderAdditionalText()}</div>
+                  </Box>
+                  <Link to="/gallery" className="buttonlink mobile-only">
+                    Shop Better Health
                   </Link>
-                  </Container>
-                </div>
-              )}
+                </Container>
+              </div>
+            )}
           </Box>
           <Box py={8} className="mobile-padding">
             <Container className="section3">
@@ -289,9 +288,7 @@ const AboutUs = ({ location }) => {
             <Container>
               <h3>Press & Media Inquires?</h3>
               <h1>
-                <a href="mailto:press@objectivewellness.com">
-                  press@objectivewellness.com
-                </a>
+                <a href="mailto:press@objectivewellness.com">press@objectivewellness.com</a>
               </h1>
             </Container>
           </Box>
@@ -299,8 +296,12 @@ const AboutUs = ({ location }) => {
       </ScrollToTop>
     </>
   ) : (
-      <></>
-    );
+    <></>
+  );
+};
+
+AboutUs.propTypes = {
+  location: PropTypes.object.isRequired
 };
 
 export default AboutUs;
