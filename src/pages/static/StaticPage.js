ca3e14fc5c032b29198f98b317d5d2026b0ce909
David Hebert <davidh@DavidHeBP162019.fios-router.home>
2020-06-29 11:37:01 -0400

    Update StaticPage to enforce routing and 404 handling

--- src/pages/static/StaticPage.js
+++ src/pages/static/StaticPage.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+
 import LoadingSpinner from '../../components/LoadingSpinner';
-import Box from '@material-ui/core/Box';
 import { requestPage } from '../../modules/static/actions';
 import { buildPage } from '../../utils/sputils';
 import NotFound from '../notfound/NotFound';
 
 const StaticPage = ({ match }) => {
   const { slug } = match.params;
+  const isLandingPage = match.path.startsWith('/landing');
+  const isBlogPost = match.path.startsWith('/journal');
   const dispatch = useDispatch();
   const [pageLoaded, setPageLoaded] = useState(false);
   const [tracked, setTracked] = useState(false);
@@ -21,31 +24,51 @@ const StaticPage = ({ match }) => {
 
   useEffect(() => {
     // check this when we have real content from content ms
-    if (page.hasOwnProperty('template')) {
-      // check for components here.
-      setPageLoaded(true);
-      if (tracked === false && page.name) {
-        setTracked(true);
-        window.analytics.page(`LP: ${page.name}`);
+    let analyticsStr;
+    if (page.template && !tracked) {
+      // Check templates against routes:
+      if (isLandingPage && page.template === 'LP-Template-1') {
+        analyticsStr = `LP: ${page.name}`;
+        setPageLoaded(true);
+      } else if (isBlogPost && page.template === 'Blog-Post') {
+        analyticsStr = 'Journal Post';
+        setPageLoaded(true);
+      } else {
+        analyticsStr = '404 Error';
+        setPageError(true);
       }
+      setTracked(true);
+    } else if (page.error) {
+      analyticsStr = '404 Error';
+      setPageError(true);
     }
-    // else {
-    //   setPageError(true);
-    // }
+    window.analytics.page(analyticsStr);
   }, [page]);
 
-  let FinalPage = null;
+  let FinalPage;
+
   if (pageLoaded) {
     FinalPage = () => buildPage(page);
   }
-  // if (pageError) {
-  //   FinalPage = () => <NotFound />;
-  // }
+
+  if (pageError) {
+    FinalPage = () => NotFound();
+  }
 
   if (FinalPage) {
     return <FinalPage />;
   }
+
   return <LoadingSpinner loadingMessage="...loading" page="lp" />;
+};
+
+StaticPage.propTypes = {
+  match: {
+    path: PropTypes.string,
+    params: {
+      slug: PropTypes.string
+    }
+  }
 };
 
 export default withRouter(StaticPage);
