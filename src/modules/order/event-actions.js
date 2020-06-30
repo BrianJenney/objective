678784d2eb705f8c060eb4bb7aa2932e8f7a0b49
Kevin <kevinch@nutranext.net>
2020-06-30 10:45:53 -0400

    sending FE event on order placement

---
+++ src/modules/order/event-actions.js
@@ -0,0 +1,40 @@
+import { FE_ORDER_SUBMITTED } from './types';
+
+const localStorageClient = require('store');
+const msgpack = require('msgpack-lite');
+const ObjectId = require('bson-objectid');
+
+export const emitOrderSubmitted = args => async (dispatch, getState) => {
+  dispatch({
+    type: FE_ORDER_SUBMITTED,
+    payload: { isLoading: true }
+  });
+
+  const { client: stompClient, replyTo } = getState().stomp;
+  const { merchantAccountId } = getState().storefront;
+
+  const params = {
+    data: { ...args },
+    params: {
+      merchantAccountId,
+      ...(localStorageClient.get('clickId') && {
+        clickId: localStorageClient.get('clickId')
+      })
+    }
+  };
+
+  const payload = JSON.stringify(msgpack.encode(params));
+  stompClient.send(
+    '/exchange/be-events/fe.order.submitted',
+    {
+      'reply-to': replyTo,
+      'correlation-id': ObjectId(),
+      token: localStorageClient.get('olympusToken')
+    },
+    payload
+  );
+  // @segment - Order Submitted Event
+  window.analytics.track('Order Submitted', {
+    cart_id: args.cartId
+  });
+};
