dd0dbc201353f8ebe1021f1536f8f79296aa7aaf
David Hebert <davidh@davidhebp162019.myfiosgateway.com>
2020-07-07 11:42:47 -0400

    Replace old Terms.js with new

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,6 @@ export { default as BlogCategory } from './BlogCategory';
 export { default as BlogTag } from './BlogTag';
 export { default as PrivacyPolicyPage } from './static/PrivacyPage';
 export { default as StaticPageOne } from './static/StaticPage';
-// export { default as TermsPage } from './Terms';
-export { default as TermsPage } from './NewTerms';
+export { default as TermsPage } from './Terms';
 export { default as NotFoundPage } from './notfound/NotFound';
 export { default as TrackOrderPage } from './TrackOrder';
