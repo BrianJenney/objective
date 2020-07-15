2fbbb1b82f4f1b1b14a5476641cce3eebb3059a1
Anne <annes@nutranext.net>
2020-07-15 11:21:54 -0700

    addresses linter errors

--- src/components/ProductSlider/PDPSlider.js
+++ src/components/ProductSlider/PDPSlider.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 import ImageGallery from 'react-image-gallery';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
@@ -35,33 +36,31 @@ const PDPSlider = props => {
       return undefined;
     }
 
-    for (let imgParent of largeImg) {
+    largeImg.forEach(imgParent => {
       imgParent.classList.add(styles['reset-display']);
       const prodImg = imgParent.getElementsByTagName('img');
-
-      for (let pic of prodImg) {
+      prodImg.forEach(pic => {
         pic.classList.add(styles['reset-image-size']);
-      }
-    }
+      });
+    });
 
-    for (let thumb of thumbImg) {
-      const thumbImg = thumb.getElementsByTagName('img');
-
-      for (let item of thumbImg) {
+    thumbImg.forEach(thumb => {
+      const thumbnailImg = thumb.getElementsByTagName('img');
+      thumbnailImg.forEach(item => {
         item.classList.add(styles['reset-thumb-height']);
-      }
-    }
+      });
+    });
 
     if (windowWidth < 769) {
-      imageWrapper[0].classList.add(styles['newWidth']);
+      imageWrapper[0].classList.add(styles.newWidth);
     } else if (windowWidth > 769) {
-      imageWrapper[0].classList.remove(styles['newWidth']);
+      imageWrapper[0].classList.remove(styles.newWidth);
     }
 
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
       if (windowWidth < 769) {
-        imageWrapper[0].classList.add(styles['newWidth']);
+        imageWrapper[0].classList.add(styles.newWidth);
       }
     };
     window.addEventListener('resize', handleResize);
@@ -88,27 +87,29 @@ const PDPSlider = props => {
     });
   };
 
-  const renderImages = images => {
-    let carouselImages = [];
+  const renderImages = imagesToRender => {
+    const carouselImages = [];
 
-    images.map(image => {
-      let imageUrlOriginal = image.fields.file.url + '?w=687&w=687&q=50';
-      let imageUrlOriginalSplit = imageUrlOriginal.split('//images.ctfassets.net/mj9bpefl6wof/');
-      imageUrlOriginal = 'https://nutranext.imgix.net/' + imageUrlOriginalSplit[1] + '&auto=compress,format';
+    imagesToRender.map(image => {
+      let imageUrlOriginal = `${image.fields.file.url}?w=687&w=687&q=50`;
+      const imageUrlOriginalSplit = imageUrlOriginal.split('//images.ctfassets.net/mj9bpefl6wof/');
+      imageUrlOriginal = `https://nutranext.imgix.net/${imageUrlOriginalSplit[1]}&auto=compress,format`;
 
-      let imageUrlThumbnail = image.fields.file.url + '?w=120&h=120&q=50';
-      let imageUrlThumbnailSplit = imageUrlThumbnail.split('//images.ctfassets.net/mj9bpefl6wof/');
-      imageUrlThumbnail = 'https://nutranext.imgix.net/' + imageUrlThumbnailSplit[1] + '&auto=compress,format';
+      let imageUrlThumbnail = `${image.fields.file.url}?w=120&h=120&q=50`;
+      const imageUrlThumbnailSplit = imageUrlThumbnail.split(
+        '//images.ctfassets.net/mj9bpefl6wof/'
+      );
+      imageUrlThumbnail = `https://nutranext.imgix.net/${imageUrlThumbnailSplit[1]}&auto=compress,format`;
 
-      {
-        !xs
-          ? carouselImages.push({
-              original: imageUrlOriginal,
-              thumbnail: imageUrlThumbnail
-            })
-          : carouselImages.push({
-              original: imageUrlOriginal
-            });
+      if (!xs) {
+        carouselImages.push({
+          original: imageUrlOriginal,
+          thumbnail: imageUrlThumbnail
+        });
+      } else {
+        carouselImages.push({
+          original: imageUrlOriginal
+        });
       }
 
       return carouselImages;
@@ -124,8 +125,8 @@ const PDPSlider = props => {
           showPlayButton={false}
           items={carouselImages}
           thumbnailPosition={xs ? 'bottom' : 'left'}
-          showThumbnails={xs ? false : true}
-          showBullets={xs ? true : false}
+          showThumbnails={!xs}
+          showBullets={!!xs}
           onSlide={handleSegmentBrowsedEvent}
         />
       </div>
@@ -133,6 +134,10 @@ const PDPSlider = props => {
   };
 
   return <>{renderImages(images)}</>;
+};
+
+PDPSlider.propTypes = {
+  images: PropTypes.array.isRequired
 };
 
 export default PDPSlider;
