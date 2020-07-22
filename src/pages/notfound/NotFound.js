92b2a713ae7c3b703fce7117c375e208c1ba7ba2
David Hebert <davidh@davidhebp162019.myfiosgateway.com>
2020-07-22 10:05:00 -0400

    Use prerender-status-code to return 404 to crawlers when NotFound is rendered

--- src/pages/notfound/NotFound.js
+++ src/pages/notfound/NotFound.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useMediaQuery } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Helmet } from 'react-helmet';
 import { NavLink } from '../../components/common';
 
 const useStyles = makeStyles({
@@ -18,7 +19,10 @@ const NotFound = () => {
 
   return (
     <>
-      <NavLink to={'/'}>
+      <Helmet>
+        <meta name="prerender-status-code" content="404" />
+      </Helmet>
+      <NavLink to="/">
         {mobile ? (
           <img
             src="https://images.ctfassets.net/mj9bpefl6wof/19ChPMduwPcvkvecLiOiXf/ebdd41cd42ed97e2249b92eb88a1d58d/404-mobile.png?w=450&fm=jpg&q=50"
@@ -26,12 +30,12 @@ const NotFound = () => {
             className={classes.root}
           />
         ) : (
-            <img
-              src="https://images.ctfassets.net/mj9bpefl6wof/3bJxwTFP6IpI9tkT5xiLZL/d3b2981571f8a26ac372f836b696f555/404_-_desktop.png?w=2000&q=50&fm=jpg"
-              alt=""
-              className={classes.root}
-            />
-          )}
+          <img
+            src="https://images.ctfassets.net/mj9bpefl6wof/3bJxwTFP6IpI9tkT5xiLZL/d3b2981571f8a26ac372f836b696f555/404_-_desktop.png?w=2000&q=50&fm=jpg"
+            alt=""
+            className={classes.root}
+          />
+        )}
       </NavLink>
     </>
   );
